152d92b8697f30d1b2c2cf8abc8d2ee8b5750ea4
Linus Kirkwood <lkirkwood@allette.com.au>
2020-12-16 11:29:37 +1100

    switched back to put method for ansible fragments: not working yet

--- fragments/ansible_info.py
+++ fragments/ansible_info.py
@@ -9,26 +9,9 @@ def main():
     path = 'Ansible/outgoing'
     section = 'ansible'
     script = 'ansible_info.py'
-    # dcount = {}
-    # for file in os.scandir(path):
-    #     docid = file.name.split(';')[0]
-    #     if docid not in dcount:
-    #         dcount[docid] = 0
-    #     dcount[docid] += 1
-    # # for docid in dcount:
-    # #     delete(docid, dcount[docid], section)
     for file in os.scandir(path):
         filepath = path + '/' + os.path.basename(file)
-        push.post(script, filepath, section)
-
-
-# def delete(docid, count, section):
-#     jession = 'JSESSIONID=FB00C026C59FFC5C6EA4C089A4BF8095'
-#     for fragment in range(count):
-#         url = 'https://ps-doc.allette.com.au/ps/service/members/~{0}/groups/~{1}/uris/~{2}/fragments/{3}/delete'.format('lkirkwood', 'network-documentation', docid, fragment)
-#         r = requests.post(url, headers={'Cookie': jession}, params={'section': section})
-#         with open('Logs/ansible_info.xml', 'w') as o:
-#             o.write(BeautifulSoup(r.text, 'lxml').prettify())
+        push.put(script, filepath, section)
 
 if __name__ == '__main__':
     main()
