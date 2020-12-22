c79ff1e0ce15ccc540d16952197e14a2fd08c1e9
Linus Kirkwood <lkirkwood@ad.allette.com.au>
2020-12-22 14:32:58 +1100

    added script to version folders of documents

---
+++ fragments/version.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+from bs4 import BeautifulSoup
+from requests import post
+import auth
+
+def set(folder):
+    token = auth.token()
+    header = {
+        'authorization': 'Bearer {0}'.format(token)
+    }
+
+    date = datetime.now().replace(microsecond=0) #drop unnecessary microseconds
+    params = {
+        'name': date
+    }
+
+    uri = urimap[folder]
+    url = 'https://ps-doc.allette.com.au/ps/service/members/~lkirkwood/groups/~network-documentation/uris/{0}/versions'.format(uri)
+
+    r = post(url, headers=header, params=params)
+    
+    with open('Logs/versionlog.xml', 'w') as log:
+        log.write(BeautifulSoup(r.text, 'lxml').prettify())
+
+
+urimap = {
+    'Hosts': '32280',
+    'IPs': '32879',
+    'Ports': '19040',
+    'AnsibleJson': '24577',
+    'Documents': '5471'
+}
+
+if __name__ == "__main__":
+    set('Hosts')
