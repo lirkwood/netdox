e518422db2d62b9b5c148a36ea0ee845baa33a72
Linus Kirkwood <lkirkwood@ad.allette.com.au>
2021-01-08 18:15:59 +1100

    fixed cnames

--- docs/dnsme_domains.py
+++ docs/dnsme_domains.py
@@ -1,103 +1,69 @@
 from bs4 import BeautifulSoup
-import pprint
+import json
 import csv
 
 ##########################################################################################################
 # takes all domains and their associated records and compiles a list of all domains and their subdomains #
 ##########################################################################################################
 def main():
-    domains = open("../Sources/domains.xml", "r")  #for top level domains
-    records = open("../Sources/records.xml", "r")  #for subdomains
-    output = open("../sources/domains.csv", "w", newline='')
-    writer = csv.writer(output)
+    cnames = {}
+    with open('../Sources/cnames.json', 'r') as stream:
+        cnames = json.load(stream)
+    with open("../Sources/domains.xml", "r") as domains:
+        with open("../Sources/records.xml", "r") as records:
+            with open("../sources/domains.csv", "a", newline='') as output:
+                writer = csv.writer(output)
 
-    rsoup = BeautifulSoup(records, features='xml')
-    dsoup = BeautifulSoup(domains, features='xml')
+                rsoup = BeautifulSoup(records, features='xml')
+                dsoup = BeautifulSoup(domains, features='xml')
 
-    type = rsoup.type   #grab all type tags
-    value = rsoup.value #all subdomain url tags
+                type = rsoup.type   #grab all type tags
+                value = rsoup.value #all subdomain url tags
 
-    global dmndict
-    global rcddict
-    dmndict = {}
-    rcddict = {}
+                dmndict = {}
+                rcddict = {}
 
-    for domain in dsoup.find_all('data'):
-        dmndict[domain.id.string.strip(' \n.')] = domain.find('name').string.strip(' \n.')    #skip dupes and make lists for subdomains
-    
-    count = 0
-    for record in rsoup.find_all('data'):
-        type = clean(record.type)
-        value = clean(record.value)
-        sourceid = clean(record.sourceId)
-        name = clean(record.find('name'))
-        if type == 'A': #for A records, build correct domain name and associate with IP
-            ip = value
-            if name:
-                parent = dmndict[sourceid]
-                domain = name + '.' + parent
-            else:
-                domain = dmndict[sourceid]
+                for domain in dsoup.find_all('data'):
+                    dmndict[domain.id.string] = domain.find('name').string    #skip dupes and make lists for subdomains
                 
-            domain = domain.replace('*.', '')
-            domain = domain.replace('www.', '')
-            count += 1
-            if domain in rcddict:
-                rcddict[domain].append(ip)
-            else:
-                rcddict[domain] = [ip]
-    # print(count)
+                for record in rsoup.find_all('data'):
+                    type = record.type.string
+                    value = record.value.string
+                    sourceid = record.sourceId.string
+                    parent = dmndict[sourceid]
+                    name = record.find('name').string
+                    if type == 'A':
+                        ip = value
+                        if name:
+                            domain = name + '.' + parent
+                        else:
+                            domain = dmndict[sourceid]
+                        
+                        domain = domain.replace('*.', '')
+                        domain = domain.replace('www.', '')
+                        if domain in rcddict:
+                            rcddict[domain].append(ip)
+                        else:
+                            rcddict[domain] = [ip]
+                    elif type == 'CNAME':
+                        if value and value.endswith('.'):
+                            dest = value.strip('.')
+                        else:
+                            dest = dmndict[sourceid]
+                            if value:
+                                dest = value +'.'+ dest
 
-    count = 0
-    for record in rsoup.find_all('data'):
-        type = clean(record.type)
-        if type == 'CNAME':
-            count += 1
-            cname(record)
-    # print(count)
+                        if dest not in cnames:
+                            cnames[dest] = []
+                        cnames[dest].append(name +'.'+ parent)
 
-    for d in rcddict:
-        for ip in rcddict[d]:
-            writer.writerow(['DNSMadeEasy', d, ip])
-
-def cname(record):
-    global dmndict
-    global rcddict
-
-    sourceid = clean(record.sourceId)
-    parent = dmndict[sourceid]
-    alias = clean(record.find('name'))
-    value = clean(record.value)
-    if value:
-        if value.endswith('.'):
-            dest = value.strip('.')
-        else:
-            dest = value + '.' + parent
-    else:
-        dest = parent
-    if alias:
-        domain = alias + '.' + parent
-    else:
-        domain = parent
-    domain = domain.replace('*.', '')
-    domain = domain.replace('www.', '')
-    if dest in rcddict:
-        ips = list(rcddict[dest])
-    else:
-        ips = None
-
-    if ips:
-        for ip in ips:
-            if domain in rcddict:
-                rcddict[domain].append(ip)
-            else:
-                rcddict[domain] = [ip]
-    
-    
-def clean(s):
-    if len(s) > 0:
-        s = s.string.strip(' \n')
-        return s
-
+                for d in rcddict:
+                    for ip in rcddict[d]:
+                        writer.writerow(['DNSMadeEasy', d, ip])
+    with open('../Sources/cnames.json', 'w') as stream:
+        for i in cnames:
+            cnames[i] = list(dict.fromkeys(cnames[i]))
+        stream.write(json.dumps(cnames, indent=4))
+             
 if __name__ == '__main__':
     main()
