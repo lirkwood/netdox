d70be4e8194d7a89a2b17708d696668b0882e11c
Linus Kirkwood <lkirkwood@ad.allette.com.au>
2021-01-15 12:56:59 +1100

    improved method of mapping workers to their hostnames and removed code for producing fragment documents

---
+++ docs/ingress2pod.py
@@ -0,0 +1,188 @@
+from bs4 import BeautifulSoup
+import copy
+import json
+import sys
+import csv
+import os
+
+
+def ingress():
+    with open('../Sources/ingress.json', 'r') as stream:
+        jsondata = json.load(stream)
+        idict = {}
+        for c in jsondata:  #context either sandbox or production cluster
+            context = jsondata[c]
+            idict[c] = {}
+            for service in context['items']:
+                name = service['metadata']['name']
+                hosts = []
+                for h in service['spec']['rules']:
+                    host = h['host'].replace('www.', '')
+                    hosts.append(host)
+
+                hosts = list(dict.fromkeys(hosts))  #make unique
+                idict[c][name] = hosts  #dict has structure 'service name' : ['host1', 'host2',...] etc
+    
+    return idict
+
+
+
+def service(sdict):
+    global links
+    ndict = {} #new dictionary
+    noingress = {}
+    links = {}
+    with open('../Sources/services.json', 'r') as stream:
+        jsondata = json.load(stream)
+        for c in jsondata:
+            ndict[c] = {}
+            noingress[c] = {}
+            context = jsondata[c]
+            for service in context['items']:
+                name = service['metadata']['name']
+                try:
+                    if 'app.kubernetes.io/instance' in service['spec']['selector']: #grab app names, some services use older formatting style
+                        app = service['spec']['selector']['app.kubernetes.io/instance']
+                    elif 'app' in service['spec']['selector']:
+                        app = service['spec']['selector']['app']
+                    else:
+                        app = None
+                        print('Found isolated service: {0}. Ignoring...'.format(name))  #if has no link at all
+                    try:
+                        ndict[c][app] = sdict[c][name]  #dict where deployment is key and associated domains are values
+                    except KeyError:
+                        print('Found service with no ingress: {0}. Attempting to find related service...'.format(name))
+                        noingress[c][name] = app
+                except KeyError:
+                    print('Found isolated service: {0}. Ignoring...'.format(name))
+        for context in noingress:   #sandbox and production
+            for service in noingress[context]:  #for each service with no matching ingress
+                selector = noingress[context][service]  #value of the service in ndict is its selector; either its deployment or sibling service
+                if selector in ndict[context].keys():   #if its sibling service has an entry
+                    siblingsdom = ndict[context][selector]  #get its sibling service's domains
+                    ndict[context][service] = siblingsdom   #associate unmatched service with sibling's domains
+                    print('Service {0} matched on service {1}'.format(service, selector))
+                    links[service] = selector   #record links
+
+    return ndict
+
+
+
+def pods(sdict):
+    global workers
+    workers = []
+    pdict = {}
+    with open('../Sources/pods.json', 'r') as stream:
+        jsondata = json.load(stream)
+        for c in jsondata:
+            pdict[c] = {}
+            context = jsondata[c]
+            for pod in context['items']:
+                labels = pod['metadata']['labels']
+                try:
+                    if 'app' in labels:
+                        appname = labels['app']
+                    else:
+                        appname = labels['app.kubernetes.io/instance']
+                except KeyError:
+                    print('Discovered system pod. Ignoring...')
+                    appname = None
+                podname = pod['metadata']['name']
+                if appname:
+                    if appname not in pdict[c]:
+                        pdict[c][appname] = {}
+                        pdict[c][appname]['pods'] = {}
+                    pdict[c][appname]['pods'][podname] = {}
+                    _pod = pdict[c][appname]['pods'][podname]
+                    nodename = pod['spec']['nodeName']
+                    hostip = pod['status']['hostIP']
+                    workers.append(nodename)
+                    try:
+                        pdict[c][appname]['nodename'] = nodename
+                        pdict[c][appname]['hostip'] = hostip
+                        pdict[c][appname]['domains'] = sdict[c][appname]
+                        containers = {}
+                        for container in pod['spec']['containers']:
+                            cname = container['name']
+                            image = container['image']
+                            containers[cname] = image
+                        _pod['containers'] = containers
+                            
+                    except KeyError:
+                        print('Discovered pod with no service {0}. Ignoring...'.format(appname))
+    
+    return pdict
+
+
+
+def pivot(pdict):   #rearrange dict so domains are the keys and deployments are values
+    temp = {}
+    for context in pdict:
+        temp[context] = {}
+        for d in pdict[context]:
+            deployment = pdict[context][d]
+            try:
+                for domain in deployment['domains']:
+                    domain = domain.replace('.internal', '')
+                    temp[context][domain] = deployment
+                    temp[context][domain].pop('domains', None)
+            except KeyError:
+                pass
+    return temp
+
+
+
+def mapworkers(dmndict):
+    global workers
+    tmp = {}
+    with open('../Sources/domains.csv', 'r') as stream:
+        for row in csv.reader(stream):
+            if row[0] != 'Kubernetes':
+                for worker in workers:
+                    if worker in row[1]:
+                        tmp[worker] = row[1]
+    workers = dict(tmp)
+
+    for context in dmndict:
+        for domain in dmndict[context]:
+            dmndict[context][domain]['worker'] = workers[dmndict[context][domain]['nodename']]
+    
+    for c in dmndict:
+        for d in dmndict[c]:
+            if 'nodename' in dmndict[c][d].keys():
+                dmndict[c][d].pop('nodename')
+    
+    return dmndict
+
+
+
+def refresh():
+    if len(sys.argv) == 2:
+        if sys.argv[1] == '-r':
+            os.system('pwsh.exe ./get-ingress.ps1')
+            os.system('pwsh.exe ./get-services.ps1')
+            os.system('pwsh.exe ./get-pods.ps1')
+        else:
+            print('Invalid argument. Accepted flags are: [-r]')
+            exit()
+    elif len(sys.argv) > 2:
+        print('Too many arguments. Accepted flags are: [-r]')
+        exit()
+
+
+
+
+def main():
+    refresh()
+    idict = ingress()
+    sdict = service(idict)
+    pdict = pods(sdict)
+    dmndict = pivot(pdict)
+    master = mapworkers(dmndict)
+    with open('../Sources/kube.json', 'w') as out:
+        out.write(json.dumps(master, indent=4))
+    
+
+
+if __name__ == '__main__':
+    main()
