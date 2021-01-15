583e4e1e348e089a9a8bb3a0f04e15885b6a23d3
Linus Kirkwood <lkirkwood@ad.allette.com.au>
2021-01-15 15:29:01 +1100

    removed old scripts

--- fragments/main.py
+++ fragments/main.py
@@ -6,27 +6,18 @@ import os
 import ansible.ansible
 import ansible_info
 
-import kube.ingress2pod
-import kube_info
-
 import reversedns.get_ptr
 import ptr_info
 
 print('Generating Ansible fragments...')
 print('****************************')
 ansible.ansible.main()
-print('**************************** \n\n')
-print('Generating Kubernetes fragments...')
-print('*******************************')
-kube.ingress2pod.main()
 print('******************************* \n\n')
 print('Generating reverse dns record fragments...\n\n')
 reversedns.get_ptr.main()
 
 print('Pushing Ansible fragments...')
 ansible_info.main()
-print('Pushing Kubernetes fragments...')
-kube_info.main()
 print('Pushing reverse dns record fragments...')
 ptr_info.main()
 
