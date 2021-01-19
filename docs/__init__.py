5ec259860668c6225a0dabb7e1176f315e1d27b8
Linus Kirkwood <lkirkwood@ad.allette.com.au>
2021-01-19 17:13:59 +1100

    moved binary.py and test.py functions into __init__.py

--- docs/__init__.py
+++ docs/__init__.py
@@ -1,3 +1,26 @@
+import re
+
+def valid_ip(string):
+    rawstring = bytes(string, 'utf-8')
+    for octet in string.split('.'):
+        if len(octet) > 3 or len(octet) < 1:
+            print('Bad octet in ip: {0}'.format(rawstring))
+            return False
+        elif int(octet) > 255 or int(octet) < 0:
+            print('Bad octet in ip: {0}'.format(rawstring))
+            return False
+
+    if string.count('.') != 3:
+        print('Wrong number of octets in ip: {0}'.format(rawstring))
+        return False
+
+    for char in re.findall(r'[^0-9.]',string):
+        print('Bad character {0} in ip: {1}'.format(char, rawstring))
+        return False
+
+    return True
+
+
 def subn_bounds(subnet):
     bounds = {}
 
@@ -16,6 +39,7 @@ def subn_bounds(subnet):
 
     return bounds
 
+
 def netbox_prefixes():
     prefixes = {}
     with open('../sources/prefixes.txt') as stream:
@@ -23,6 +47,7 @@ def netbox_prefixes():
             if line not in prefixes:
                 prefixes[line] = subn_bounds(line)
     return prefixes
+
 
 def netbox_sort(ip):
     bin_ip = int(binaryip(ip), 2)
@@ -37,12 +62,14 @@ def netbox_sort(ip):
         # print('No prefix match for ip {0}. Sorted using default method...'.format(ip))
         return '.'.join(ip.split('.')[:3]) + '.0/24'
 
+
 def binaryip(ip):
     bin_ip = ''
     for octet in ip.split('.'):
         bin_ip += bin(int(octet))[2:].zfill(8) 
        #strip 0b prefix and pad to size with 0s
     return bin_ip
+
 
 def in_subnet(ip, subnet, verbose=False):
     bin_ip = int(binaryip(ip), base=2)
@@ -55,6 +82,3 @@ def in_subnet(ip, subnet, verbose=False):
         if verbose:
             print('IP Address {0} is outside subnet {1}.'.format(ip, subnet))
         return False
-
-#usage: call netbox_sort() to sort an IPv4 address into subnets defined in prefixes.txt
-#       call binaryip() for a binary representation of an IPv4 address.
