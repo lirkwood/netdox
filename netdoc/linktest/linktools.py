aabe3666dddb8facf98af4ae42d000ed77c5634d
Linus Kirkwood <lkirkwood@ad.allette.com.au>
2021-01-22 16:23:57 +1100

    added timeout and removed unnecessary get_cnames()

--- netdoc/linktest/linktools.py
+++ netdoc/linktest/linktools.py
@@ -3,6 +3,7 @@ import subprocess
 import requests
 import json
 import auth
+import os
 
 
 token = auth.token()
@@ -12,7 +13,7 @@ header = {
 base = 'https://ps-doc.allette.com.au/ps/service'
 
 urimap = {
-    'dns': 	'42416',
+    'dns': 	'46055',
     'ips': '45604',
     'deployments': '42262',
     'xo': '42183'
@@ -22,7 +23,7 @@ urls = []   #list of urls to be tested
 
 global count    #no. of urls tested
 count = 0  
-live = []       #no. of successful responses to a basic GET
+live = []      #no. of successful responses to a basic GET
 dead = {}       #key = url, value = error code
 
 def main(folder):
@@ -34,29 +35,31 @@ def main(folder):
     with open('log.txt','w') as log:
         log.write('Out of {0} tested urls, {1} failed.\n'.format(count, len(dead)))
         for url in dead:
-            log.write('URL: {0} Code: {1}\n'.format(url, dead[url]))
+            log.write('URL: {0} Code: {1}\n\n'.format(url, dead[url]))
     with open('live.json','w') as out:
         out.write(json.dumps(live, indent=2))
     
+    if not os.path.exists('outgoing'):
+        os.mkdir('outgoing')
+    else:
+        for file in os.scandir('outgoing'):
+            os.remove(file)
+            
     subprocess.run('node screenshot.js')    #get screenshots of all urls in live
+
+    for file in os.scandir('outgoing'):
+        docid = '_nd_'+ file.name.split('.')[0]
+        print(docid)
 
 
 def get_uris(folder): #returns list of uris of all documents in a folder, defined by urimap
     service = '/groups/~network-documentation/uris/{0}/uris'.format(urimap[folder])
     r = requests.get(base+service, headers=header, params={'pagesize': 9999})
     soup = BeautifulSoup(r.text, features='xml')
-    for uri in soup.find_all('uri'):
-        get_cnames(uri['docid'])
-
-
-def get_cnames(uri): #queries ps for the content of the 'aliases' fragment
-    service = '/members/~lkirkwood/groups/~network-documentation/uris/{0}/fragments/{1}'.format(uri,'aliases')
-    r = requests.get(base+service, headers=header)
-    soup = BeautifulSoup(r.text, features='xml')
+    for document in soup.find_all('uri'):
+        url = 'https://'+ document['docid'].replace('_nd_','').replace('_','.')
+        urls.append(url)
     
-    for property in soup.find_all('property'):
-        urls.append(property['value'])
-    urls.append('https://'+ soup.uri['title'])
         
 
 def test(url):
@@ -69,7 +72,7 @@ def test(url):
         if '_ssl.c:1123' in str(e) and 'https' in url:
             try:
                 if alive(url.replace('https','http')):
-                    print('HTTPS failed but HTTP succeeded.')
+                    print('HTTPS failed but HTTP succeeded.\n\n')
                     dead[url] = 'HTTPS failed but HTTP succeeded.'
             except Exception as e:
                 print('Fatal error occurred: {0}\n\n'.format(str(e)))
@@ -80,7 +83,7 @@ def test(url):
 
 
 def alive(url):
-    r = requests.get(url)
+    r = requests.get(url, timeout=3)
     if r.status_code > 400 and r.status_code < 600:
         print('Bad response code {0} from url {1}\n\n'.format(r.status_code, url))
         dead[url] = r.status_code
