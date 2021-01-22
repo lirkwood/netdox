2d0a8f1af5d8a43acc6c7a777467bb5bb9fb6d44
Linus Kirkwood <lkirkwood@ad.allette.com.au>
2021-01-22 16:24:42 +1100

    .

--- netdoc/get-workers.ps1
+++
@@ -1,9 +0,0 @@
-kubectl config use-context sandbox
-$sandbox = kubectl get nodes --selector=node-role.kubernetes.io/worker -o json | ConvertFrom-Json
-kubectl config use-context production
-$production = kubectl get nodes --selector=node-role.kubernetes.io/worker -o json -o json | ConvertFrom-Json
-
-$dict = @{}
-$dict['sandbox'] = $sandbox
-$dict['production'] = $production
-$dict | ConvertTo-Json -depth 100 | Out-File Sources/workers.json
