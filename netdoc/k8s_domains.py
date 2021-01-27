6cc0fd92977172a7ea3aa8e2378e629c0558cff4
Linus Kirkwood <lkirkwood@ad.allette.com.au>
2021-01-27 17:50:20 +1100

    new script k8s_domains.py for getting domains unique to k8s dns

---
+++ netdoc/k8s_domains.py
@@ -0,0 +1,19 @@
+import json
+import os
+
+def main():
+    with open('Sources/apps.json','r') as stream:
+        master = {}
+        jsondata = json.load(stream)
+        for context in jsondata:
+            for _app in jsondata[context]:
+                app = jsondata[context][_app]
+                try:
+                    for domain in app['domains']:
+                        if domain not in master:
+                            master[domain] = {'dest': {'ips': [], 'domains': [], 'apps': []}, 'root': '', 'source': 'Kubernetes'}
+                        master[domain]['dest']['apps'].append(_app)
+                except KeyError:
+                    pass
+        
+        return master
