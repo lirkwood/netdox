a0e2eb3df393d8188cac55f3ac4f39bbde7c0a31
Linus Kirkwood <lkirkwood@ad.allette.com.au>
2021-02-12 11:13:01 +1100

    added error handling and checking for authentication details

--- netdox.ps1
+++ netdox.ps1
@@ -1,7 +1,94 @@
-Write-Host "Querying ActiveDirectory"
+function choosek8s {
+    $choice = Read-Host "No Kubernetes config detected. Provide now? (y/n)"
+    if ($choice.ToLower() -eq "y") {
+        $kubepath = Read-Host "Enter the path of the .kube folder"
+        if ((Test-Path -Path $kubepath) -eq 'True') {
+            return $kubepath
+        }
+        else {
+            Write-Host "Invalid path."
+            choosek8s
+        }
+    }
+    elseif ($choice.ToLower() -eq "n") {
+        Write-Host "Proceeding without Kubernetes information..."
+        return $null
+    }
+    else {
+        Write-Host "Invalid input detected."
+        choosek8s
+    }
+}
+
+function chooseAD {
+    $choice = Read-Host "ActiveDirectory query failed. Proceed anyway? (y/n)"
+    if ($choice.ToLower() -eq 'y') {
+        Write-Host "Proceeding..."
+    }
+    elseif ($choice.ToLower() -eq 'n') {
+        exit
+    }
+    else {
+        Write-Host "Invalid input detected."
+        chooseAD
+    }
+}
+
+function chooseAuth($name) {
+    $choice = Read-Host "Missing or incomplete $name authentication details found in 'authentication.json'. Proceed anyway? (y/n)"
+    if ($choice.ToLower() -eq 'y') {
+        Write-Host "Proceeding..."
+    }
+    elseif ($choice.ToLower() -eq 'n') {
+        exit
+    }
+    else {
+        Write-Host "Invalid input detected."
+        chooseAuth($name)
+    }
+}
+
+
+$auth = Get-Content -Path "authentication.json" | ConvertFrom-Json
+
+if (($auth.DNSMadeEasy.API -eq '') -or ($auth.DNSMadeEasy.Secret -eq '')) {
+    chooseAuth('DNSMadeEasy')
+}
+if (($auth.XenOrchestra.Username -eq '') -or ($auth.XenOrchestra.Password -eq '')) {
+    chooseAuth('Xen Orchestra')
+}
+
+
+$kubepath = Get-ChildItem $HOME ".kube" -Recurse -Directory -ErrorAction SilentlyContinue
+if ($null -eq $kubepath) {
+    $kubepath = choosek8s
+    if ($null -ne $kubepath) {
+        Write-Host "Kubernetes config detected."
+    }
+}
+else {
+    Write-Host "Kubernetes config detected."
+}
+
+
+Write-Host "Querying ActiveDirectory..."
 ./get-ad.ps1
-Write-Host "Building Docker image"
-docker build -t netdox .
+if ($? -eq 'True') {
+    Write-Host "ActiveDirectory query successful."
+}
+else {
+    chooseAD
+}
+
+
+Write-Host "Building Docker image..."
+if ($null -ne $kubepath) {
+    docker build -t netdox --build-arg kubepath=$kubepath .
+}
+else {
+    docker build -t netdox .
+}
+
 if ($? -eq 'True') {
     Write-Host "Build successful. Running container with name 'netdox-container'."
     docker run -it --name netdox-container netdox
