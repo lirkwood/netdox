d4d6623a579f5db4244cbc1a4b51c32765c08958
Linus Kirkwood <lkirkwood@ad.allette.com.au>
2021-02-23 13:43:43 +1100

    incorporated nat from another source

--- netdox/nat.py
+++ netdox/nat.py
@@ -1,19 +1,19 @@
 import json, re
 import iptools
 
-patt_nat = re.compile(r'(?P<alias>(\d{1,3}\.){3}\d{1,3}):\d+\s+(?P<dest1>(\d{1,3}\.){3}\d{1,3})-(?P<dest2>(\d{1,3}\.){3}\d{1,3}).*')
+patt_nat = re.compile(r'(?P<alias>(\d{1,3}\.){3}\d{1,3}).+?(?P<dest>(\d{1,3}\.){3}\d{1,3}).*')
 
 with open('src/nat.txt','r') as stream:
     natDict = {}
     for line in stream.read().splitlines():
         match = re.match(patt_nat, line)
-        
-        alias = iptools.parsed_ip(match['alias'])
-        dest = iptools.parsed_ip(match['dest1'])
-        if alias.ipv4 in natDict:
-            print(f'[WARNING][nat.py] {alias.ipv4} has multiple destinations in the NAT ({dest.ipv4}, {natDict[alias.ipv4]}, ...?)')
-            print(f'[INFO][nat.py] Defaulting to last discovered destination: {dest.ipv4}')
-        natDict[alias.ipv4] = dest.ipv4
+        if match:
+            alias = iptools.parsed_ip(match['alias'])
+            dest = iptools.parsed_ip(match['dest'])
+            if alias.ipv4 in natDict:
+                print(f'[WARNING][nat.py] {alias.ipv4} has multiple destinations in the NAT ({dest.ipv4}, {natDict[alias.ipv4]}, ...?)')
+                print(f'[INFO][nat.py] Defaulting to last discovered destination: {dest.ipv4}')
+            natDict[alias.ipv4] = dest.ipv4
 
 def lookup(ip):
     if ip in natDict:
