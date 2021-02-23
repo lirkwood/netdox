47323851f1a931d21bfe99e60957884108ae8fb7
Linus Kirkwood <lkirkwood@ad.allette.com.au>
2021-02-23 12:16:16 +1100

    added getSecrets query

--- netdox/secret_api.py
+++ netdox/secret_api.py
@@ -8,10 +8,16 @@ headers = {
         'Content-Type': 'text/xml',
 }
 
-def searchSecrets(term=None, field=None):
+def searchSecrets(term=None, field=None, settings={}):
     with open('src/soap.xml','r') as stream:
         soup = BeautifulSoup(stream.read(), features='xml')  #body and envelope
         soup.token.string = auth()
+
+        for setting in settings:
+            t = soup.new_tag(setting)
+            t.string = settings[setting]
+            soup.param.insert_after(t)
+
         if not field:
             soup.operation.name = 'SearchSecrets'
         else:
@@ -19,7 +25,25 @@ def searchSecrets(term=None, field=None):
             soup.operation.name = 'SearchSecretsByFieldValue'
             soup.fieldName.string = field
         soup.param.name = 'searchTerm'
+
         if term:
             soup.searchTerm.string = term
+        r = requests.post(api, headers=headers, data=bytes(str(soup), encoding='utf-8'))
+        return r
+
+def getSecret(id, settings={}):
+    with open('soap.xml','r') as stream:
+        soup = BeautifulSoup(stream.read(), features='xml')  #body and envelope
+        soup.token.string = auth()
+        
+        for setting in settings:
+            t = soup.new_tag(setting)
+            t.string = settings[setting]
+            soup.param.insert_after(t)
+
+        soup.operation.name = 'GetSecret'
+        soup.param.name = 'secretId'
+        soup.secretId.string = str(id)
+
         r = requests.post(api, headers=headers, data=bytes(str(soup), encoding='utf-8'))
         return r
