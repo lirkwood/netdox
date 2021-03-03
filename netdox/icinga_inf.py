683eeefa29859d0a2c3251c7a71db3aa64c7b520
Linus Kirkwood <lkirkwood@ad.allette.com.au>
2021-03-03 18:20:38 +1100

    moved icinga_log.json

--- netdox/icinga_inf.py
+++ netdox/icinga_inf.py
@@ -7,7 +7,7 @@ headers = {
 r = requests.get('https://icinga.allette.com.au:5665/v1/objects/hosts', auth=('root','8a2b3a827f09c18c'), verify=False)
 
 response = json.loads(r.text)
-with open('icinga_log.json','w') as stream:
+with open('src/icinga_log.json','w') as stream:
     stream.write(json.dumps(response, indent=2))
 
 def lookup(list):
