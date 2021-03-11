ff224ab1b573641a81e54f7f9b45f5fbb4cf52cd
Linus Kirkwood <lkirkwood@ad.allette.com.au>
2021-03-11 17:23:42 +1100

    removed echo

--- netdox.ps1
+++ netdox.ps1
@@ -92,7 +92,6 @@ else {
 Set-Location -Path ".kube"
 $kubeconfig_array = Get-ChildItem -Path "." -Filter "*config*" -File -Recurse | % {Resolve-Path -Relative -Path $_} | % {"/usr/.kube/${_}:" -replace '\\','/' -replace '/./','/'}
 $KUBECONFIG = -join $kubeconfig_array
-echo $KUBECONFIG
 
 # Awful pipeline that adds all files in .kube to string and converts to absolute posix path in dir /usr/.kube/
 Set-Location ".."
