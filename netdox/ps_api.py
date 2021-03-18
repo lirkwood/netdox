6b54679d2eb8cebd3c58630b256a490452f7b958
Linus Kirkwood <lkirkwood@ad.allette.com.au>
2021-03-18 16:00:10 +1100

    split get_uri into get_uris and get_uri also export and get_thread now return text response

--- netdox/ps_api.py
+++ netdox/ps_api.py
@@ -20,33 +20,40 @@ def auth():
         print('[ERROR][ps_api.py] PageSeeder authentication failed.')
         quit()
 
-def get_uri(uri, params={}):
-    params['pagesize'] = 9999
 
-    service = f'/groups/~operations-network/uris/{uri}/uris'
+def get_uri(uri, group='operations-network'):
+    service = f'/groups/~{group}/uris/{uri}'
+    r = requests.get(base+service, headers=header)
+    return r.text
+
+def get_uris(uri, group='operations-network', params={}):
+    if 'pagesize' not in params:
+        params['pagesize'] = 9999
+
+    service = f'/groups/~{group}/uris/{uri}/uris'
     r = requests.get(base+service, headers=header, params=params)
     return r.text
 
-def get_files(uri): # returns list of filenames in a folder on pageseeder
+
+def get_files(uri, group='operations-network'): # returns list of filenames in a folder on pageseeder
     files = []
-    soup = BeautifulSoup(get_uri(uri, {'type': 'document'}), features='xml')
+    soup = BeautifulSoup(get_uri(uri, group, {'type': 'document'}), features='xml')
     for uri in soup.find_all('uri'):
         files.append(uri['path'].split('/')[-1])
     
     return files
 
-def export(uri, params={}, path='/opt/app/src/psml'):
+
+def export(uri, params={}):
     service = f'/members/{credentials["username"]}/uris/{uri}/export'
-    if not os.path.exists(path):
-        os.mkdir(path)
     r = requests.get(base+service, headers=header, params=params)
-    return r
+    return r.text
 
 
 def get_thread(id):
     service = f'/threads/{id}/progress'
     r = requests.get(base+service, headers=header)
-    return r
+    return r.text
 
 
 
@@ -60,7 +67,10 @@ def version(uri):
     service = f'/members/~{credentials["username"]}/groups/~network-documentation/uris/{uri}/versions'
     requests.post(base+service, headers=header, params={'name': datetime.now().replace(microsecond=0)})   # version all docs that are not archived => current
 
-# Global vars
+
+###############
+# Global vars #
+###############
 
 with open('src/authentication.json','r') as stream:
     credentials = json.load(stream)['pageseeder']
