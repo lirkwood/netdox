cde1f2aa93c983642795bd86a842ad2db7dcaf69
Linus Kirkwood <lkirkwood@ad.allette.com.au>
2021-04-06 12:19:23 +1000

    dded data decrypt in init.sh

--- netdox/init.sh
+++ netdox/init.sh
@@ -2,4 +2,10 @@
 
 cp -r /etc/ext/* /opt/app/src
 chmod 777 /opt/app/*
+
+for file in "/opt/app/src/records/*.bin"; do
+    openssl enc -aes-256-cbc -d -in "$file" \
+    -K ${OPENSSL_KEY} -iv $(cat '/opt/app/src/records/vector.txt') -out "${file%.bin}"
+done
+
 python3 generate.py
