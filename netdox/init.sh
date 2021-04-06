bceb960db2d18dffd4ffc4221ccf36e6140d76e3
Linus Kirkwood <lkirkwood@ad.allette.com.au>
2021-04-06 14:22:33 +1000

    suppressed warning

--- netdox/init.sh
+++ netdox/init.sh
@@ -5,7 +5,7 @@ chmod 777 /opt/app/*
 
 for file in /opt/app/src/records/*.bin; do
     openssl enc -aes-256-cbc -d -in "$file" \
-    -K ${OPENSSL_KEY} -iv $(cat '/opt/app/src/records/vector.txt') -out "${file%.bin}" > /dev/null
+    -K ${OPENSSL_KEY} -iv $(cat '/opt/app/src/records/vector.txt') -out "${file%.bin}" &> /dev/null
 done
 
 python3 generate.py
