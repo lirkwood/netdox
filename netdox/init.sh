3c5f5be79d61b303002597a4dce8d6f159093b14
Linus Kirkwood <lkirkwood@ad.allette.com.au>
2021-04-08 17:53:02 +1000

    merged init script from dev

--- netdox/init.sh
+++ netdox/init.sh
@@ -6,12 +6,15 @@ chmod 777 /opt/app/*
 
 mkdir /opt/app/src/records
 for file in /etc/nfs/*.bin; do
-    filename=$(basename ${file%.bin})
     openssl enc -aes-256-cbc -d -in "$file" \
-    -K ${OPENSSL_KEY} -iv $(cat '/etc/nfs/vector.txt') -out "/opt/app/src/records/${filename}.json" &> /dev/null
+    -K ${OPENSSL_KEY} -iv $(cat '/etc/nfs/vector.txt') -out "/opt/app/src/records/$(basename ${file%.bin}).json" &> /dev/null
 done
 
-openssl enc -aes-256-cbc -d -in '/etc/ext/authentication.bin' \
--K ${OPENSSL_KEY} -iv $(printf authivpassphrase | xxd -p) -out '/opt/app/src/authentication.json'
+for file in /etc/ext/*.bin; do
+    openssl enc -aes-256-cbc -d -in "$file" \
+    -K ${OPENSSL_KEY} -iv $(printf authivpassphrase | xxd -p) -out "/opt/app/src/$(basename ${file%.bin})"
+done
 
-python3 generate.py
+python3 generate.py 2>&1 | tee /var/log/netdox.log
+
+cp /var/log/netdox.log /etc/ext/log/$(date '+%Y-%m-%d_%H:%M:%S')
