a710c8ef748e3298e5122175d7fa5531620ea973
Linus Kirkwood <lkirkwood@ad.allette.com.au>
2021-04-12 18:17:05 +1000

    optimised license reading

--- netdox/license_inf.py
+++ netdox/license_inf.py
@@ -5,42 +5,36 @@ import ps_api, utils
 
 license_pattern = re.compile(r'(REPLACED )?(?P<domain>[\w.-]+)\s+-\s+')
 
-def read(uri, dns):
-    id = uri['id']
+def read(uri_summary, dns):
+    id = uri_summary['id']
 
-    title_match = re.match(license_pattern, uri['title'])
+    title_match = re.match(license_pattern, uri_summary['title'])
     if title_match:
         domains = [title_match['domain']]
     else:
-        domains = [uri['title'].split()[0]]
+        domains = [uri_summary['title'].split()[0]]
 
     try:
-        subprocess.check_output(f'ping -c 1 -w 2 {domains[0]}', shell=True, stderr=subprocess.DEVNULL)
-    except subprocess.CalledProcessError:
-        domains[0] = "[old] "+ domains[0]
-    else:
-        try:
-            for alias in (dns[domains[0]].domains):
-                domains.append(alias)
-                # print(f'[INFO][license_inf.py] {domains[0]} matched on {alias}')
-        except KeyError:
-            domains[0] = "[ext] "+ domains[0]
-    finally:
-        return (id, domains)
+        for alias in (dns[domains[0]].domains):
+            domains.append(alias)
+            # print(f'[INFO][license_inf.py] {domains[0]} matched on {alias}')
+    except KeyError:
+        domains[0] = "[ext] "+ domains[0]
+        
+    return (id, domains)
     
-@utils.handle
+
 def fetch(dns):
     license_dict = {}
     # 187062 is the uri of the license folder in operations-license shared to operations-network
     license_xml = BeautifulSoup(ps_api.get_uris('187062'), 'lxml')
     for uri in license_xml("uri"):
-        uri_inf = read(uri, dns)
-        license_dict[uri_inf[0]] = uri_inf[1]
+        id, domains = read(uri, dns)
+        license_dict[id] = domains
     
     return license_dict
 
 
-@utils.handle
 def org(license_id):
     license_psml = BeautifulSoup(ps_api.get_fragment(license_id, '2'), 'lxml')
     org_id = license_psml.find(title='Organization').xref["uriid"]
