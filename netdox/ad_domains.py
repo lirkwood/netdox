436e625f6df437ed8c39e686251d96d96bfe2372
Linus Kirkwood <lkirkwood@ad.allette.com.au>
2021-04-14 15:50:21 +1000

    began refactor

--- netdox/ad_domains.py
+++ netdox/ad_domains.py
@@ -1,3 +1,4 @@
+from json.decoder import JSONDecodeError
 import os
 import json
 import iptools
@@ -7,76 +8,89 @@ import utils
 def main():
     forward = {}
     reverse = {}
+    for file in fetchJson():
+        with open(file, 'r') as stream:
+            try:
+                jsondata = json.load(stream)
+            except JSONDecodeError:
+                print(f'[ERROR][ad_domains.py] Failed to parse file as json: {file.name}')
+            else:
+                _forward, _reverse = extract(jsondata)
+
+def fetchJson():
     for file in os.scandir("src/records/"):
         if file.name.endswith('.json'):
-            try:
-                with open(file,'r') as source:
-                    jsondata = json.load(source)    #load json file
-                    for record in jsondata:
-                        if record['RecordType'] == 'A': 
-                            hostnamestr = record['DistinguishedName'].split(',')    #get hostname
-                            subdomain = hostnamestr[0].replace('DC=', '') #extract subdomain
-                            domain = hostnamestr[1].replace('DC=', '')    #extract top level domain
-                            if subdomain == '@':
-                                hostname = domain
-                            elif domain in subdomain:
-                                hostname = subdomain
-                            else:
-                                hostname = subdomain + '.' + domain
+            yield file
 
-                            hostname = hostname.replace('*.','_wildcard_')
+@utils.critical
+def extract(jsondata):
+    forward = {}
+    reverse = {}
+    for record in jsondata:
+        if record['RecordType'] == 'A':
 
-                            for item in record['RecordData']['CimInstanceProperties']:
-                                if item['Name'] == "IPv4Address":
-                                    ip = item['Value'].strip('.')
+        elif record['RecordType'] == 'CNAME':
+            domain = record['DistinguishedName'].split(',')[1].strip('DC=')
+            subdomain = record['DistinguishedName'].split(',')[0].strip('DC=')
+            
+            if subdomain == '@':
+                hostname = domain
+            elif subdomain == '*.':
+                hostname = '_wildcard_.'+ domain
+            else:
+                hostname = subdomain +'.'+ domain
+            
+            dest = ''
+            for item in record['RecordData']['CimInstanceProperties']:
+                if item['Name'] == "HostNameAlias":
+                    dest = item['Value']
+                    if not dest.endswith('.'):
+                        dest += '.'+ domain
+                    else:
+                        dest = dest.strip('.')
+        
+            if hostname not in forward:
+                forward[hostname] = utils.dns(hostname, source='ActiveDirectory', root=domain)
+            forward[hostname].link(dest, 'domain')
+        
+        elif record['RecordType'] == 'PTR':
+            zone = record['DistinguishedName'].split(',')[1].strip('DC=')
+            subnet = '.'.join(zone.replace('.in-addr.arpa','').split('.')[::-1])    #strip '.in-addr.arpa' and fix...
+            address = record['DistinguishedName'].split(',')[0].strip('DC=')        #... backwards subnet.
+            ip = iptools.ipv4(subnet +'.'+ address)
 
-                            if hostname not in forward:
-                                forward[hostname] = utils.dns(hostname, source='ActiveDirectory', root=domain)
-                            forward[hostname].link(ip, 'ipv4')
+            for item in record['RecordData']['CimInstanceProperties']:
+                if item['Name'] == 'PtrDomainName':
+                    dest = item['Value'].strip('.')
 
-                        elif record['RecordType'] == 'CNAME':
-                            domain = record['DistinguishedName'].split(',')[1].strip('DC=')
-                            subdomain = record['DistinguishedName'].split(',')[0].strip('DC=')
-                            
-                            if subdomain == '@':
-                                hostname = domain
-                            elif subdomain == '*.':
-                                hostname = '_wildcard_.'+ domain
-                            else:
-                                hostname = subdomain +'.'+ domain
-                            
-                            dest = ''
-                            for item in record['RecordData']['CimInstanceProperties']:
-                                if item['Name'] == "HostNameAlias":
-                                    dest = item['Value']
-                                    if not dest.endswith('.'):
-                                        dest += '.'+ domain
-                                    else:
-                                        dest = dest.strip('.')
-                        
-                            if hostname not in forward:
-                                forward[hostname] = utils.dns(hostname, source='ActiveDirectory', root=domain)
-                            forward[hostname].link(dest, 'domain')
-                        
-                        elif record['RecordType'] == 'PTR':
-                            zone = record['DistinguishedName'].split(',')[1].strip('DC=')
-                            subnet = '.'.join(zone.replace('.in-addr.arpa','').split('.')[::-1])    #strip '.in-addr.arpa' and fix...
-                            address = record['DistinguishedName'].split(',')[0].strip('DC=')        #... backwards subnet.
-                            ip = iptools.ipv4(subnet +'.'+ address)
-
-                            for item in record['RecordData']['CimInstanceProperties']:
-                                if item['Name'] == 'PtrDomainName':
-                                    dest = item['Value'].strip('.')
-
-                            if ip.valid:
-                                if ip.ipv4 not in reverse:
-                                    reverse[ip.ipv4] = []
-                                reverse[ip.ipv4].append(dest)
-            except Exception as e:
-                print(f'[ERROR][ad_domains.py] Failed to parse file as json: {file.name}. Exception:')
-                print(e)
-                print('[ERROR][ad_domains.py] ****END****')
+            if ip.valid:
+                if ip.ipv4 not in reverse:
+                    reverse[ip.ipv4] = []
+                reverse[ip.ipv4].append(dest)
     return (forward, reverse)
+
+def add_A(record, dns_set): 
+    hostnamestr = record['DistinguishedName'].split(',')    #get hostname
+    subdomain = hostnamestr[0].replace('DC=', '') #extract subdomain
+    root = hostnamestr[1].replace('DC=', '')    #extract root domain
+
+    # combine subdomain and root into fqdn
+    if subdomain == '@':
+        fqdn = root
+    elif subdomain == '*':
+        fqdn = '_wildcard_.' + root
+    elif root in subdomain:
+        fqdn = subdomain
+    else:
+        fqdn = subdomain + '.' + root
+
+    for item in record['RecordData']['CimInstanceProperties']:
+        if item['Name'] == "IPv4Address":
+            ip = item['Value'].strip('.')
+
+    if fqdn not in dns_set:
+        dns_set[fqdn] = utils.dns(fqdn, source='ActiveDirectory', root=root)
+    dns_set[fqdn].link(ip, 'ipv4')
 
 
 if __name__ == '__main__':
