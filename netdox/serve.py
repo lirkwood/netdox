6b316ab50436915db762533acaed56dfb0ebd095
Linus Kirkwood <lkirkwood@ad.allette.com.au>
2021-04-22 15:46:37 +1000

    added /webhooks

--- netdox/serve.py
+++ netdox/serve.py
@@ -1,11 +1,20 @@
-#!/usr/bin/env python
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def root():
     return '200 OK'
 
-@app.route('/testing')
-def test():
-    return '200 OK nice :)'
+@app.route('/webhooks', methods=['POST', 'GET'])
+def webhooks():
+    params = request.args
+    if request.content_length and request.content_length < 10**6:
+        jsondata = request.get_json()
+        if jsondata:
+            return '200 OK json parsed'
+    return '200 OK'
+
+
+# with app.test_request_context('/webhooks', method='POST'):
+#     assert request.path == '/webhooks'
+#     assert request.method == 'POST'
