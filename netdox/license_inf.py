e318cd810d5d6f2a641f548d92ae8126ecf8fea1
Linus Kirkwood <lkirkwood@ad.allette.com.au>
2021-05-07 17:42:46 +1000

    simplified

--- netdox/license_inf.py
+++ netdox/license_inf.py
@@ -1,36 +1,23 @@
 from bs4 import BeautifulSoup
-import subprocess, re
-import ps_api, utils
+import ps_api, re
 
 
 license_pattern = re.compile(r'(REPLACED )?(?P<domain>[\w.-]+)\s+-\s+')
 
-def read(uri_summary, dns):
-    id = uri_summary['id']
-
-    title_match = re.match(license_pattern, uri_summary['title'])
-    if title_match:
-        domains = [title_match['domain']]
-    else:
-        domains = [uri_summary['title'].split()[0]]
-
-    try:
-        for alias in (dns[domains[0]].domains):
-            domains.append(alias)
-            # print(f'[INFO][license_inf.py] {domains[0]} matched on {alias}')
-    except KeyError:
-        domains[0] = "[ext] "+ domains[0]
-        
-    return (id, domains)
-    
-
-def fetch(dns):
+def fetch():
     license_dict = {}
     # 187062 is the uri of the license folder in operations-license shared to operations-network
     license_xml = BeautifulSoup(ps_api.get_uris('187062'), 'lxml')
     for uri in license_xml("uri"):
-        id, domains = read(uri, dns)
-        license_dict[id] = domains
+        id = uri['id']
+
+        title_match = re.match(license_pattern, uri['title'])
+        if title_match:
+            domain = title_match['domain']
+        else:
+            domain = uri['title'].split()[0]
+
+        license_dict[domain] = id
     
     return license_dict
 
