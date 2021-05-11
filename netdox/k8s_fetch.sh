3d561936b615bd8589ed68a6aff8370ad4e9370c
Linus Kirkwood <lkirkwood@ad.allette.com.au>
2021-05-11 12:05:02 +1000

    added handling bad response from cluster

--- netdox/k8s_fetch.sh
+++ netdox/k8s_fetch.sh
@@ -7,8 +7,9 @@ contexts=($(kubectl config get-clusters | tail -n +2))
 json='{'
 for context in ${contexts[@]}
 do
-    kubectl config use-context $context
-    json+="\"$context\": $(kubectl get $resource $options -o json),"
+    kubectl config use-context $context &&\
+    response=$(kubectl get $resource $options -o json) &&\
+    json+="\"$context\": ${response},"
 done
 json=${json%,}
 json+='}'
