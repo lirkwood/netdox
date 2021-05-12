c75dbb0c90ba9a2d37404c9c5abfcb95cfecb026
Linus Kirkwood <lkirkwood@ad.allette.com.au>
2021-05-12 15:32:58 +1000

    added giving locations to clusters and therefore apps

--- netdox/k8s_domains.py
+++ netdox/k8s_domains.py
@@ -1,6 +1,5 @@
 import utils
 import json
-import os
 
 @utils.critical
 def main():
@@ -15,6 +14,10 @@ def main():
                         if domain not in master:
                             master[domain] = utils.dns(domain, source='Kubernetes')
                         master[domain].link(_app, 'app')
+                        if 'locations' in utils.auth['kubernetes'][context] and utils.auth['kubernetes'][context]['locations']:
+                            master[domain].location = utils.auth['kubernetes'][context]['locations'][0]
+                        else:
+                            print(f'[WARNING][k8s_domains.py] Cluster {context} has no location data.')
                 except KeyError:
                     pass
         
