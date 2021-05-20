89c348b6d9b22813bc81af0d709da4b0fe454110
Linus Kirkwood <lkirkwood@ad.allette.com.au>
2021-05-20 10:09:52 +1000

    plugins now have dns info

--- netdox/pluginmaster.py
+++ netdox/pluginmaster.py
@@ -15,23 +15,24 @@ def fetchRunners() -> Generator[Tuple[Any, os.DirEntry], Any, Any]:
                 yield plugin.runner, plugindir
 
 @utils.critical
-def runPlugins(dns_set: dict[str, utils.dns]):
+def runPlugins(forward_dns: dict[str, utils.dns], reverse_dns: dict[str, utils.dns]):
     for runner, plugindir in fetchRunners():
         pluginName = plugindir.name
         print(f'[INFO][pluginmaster] Discovered plugin {pluginName}')
         try:
-            dnslinks  = runner()
+            dnslinks_f, dnslinks_r = runner(forward_dns, reverse_dns)
         except Exception:
             print(f'[ERROR][pluginmaster] Running {pluginName} threw an exception: \n{format_exc()}')
         else:
-            for name, locator in dnslinks.items():
-                if name in dns_set:
-                    dns = dns_set[name]
-                    if isinstance(locator, str):
-                        dns.link(locator, pluginName)
-                    elif isinstance(locator, Iterable):
-                        for _locator in locator:
-                            dns.link(_locator, pluginName)
+            for dns_set, linkset in [(forward_dns, dnslinks_f),(reverse_dns, dnslinks_r)]:
+                for name, locator in linkset.items():
+                    if name in dns_set:
+                        dns = dns_set[name]
+                        if isinstance(locator, str):
+                            dns.link(locator, pluginName)
+                        elif isinstance(locator, Iterable):
+                            for _locator in locator:
+                                dns.link(_locator, pluginName)
 
 if __name__ == '__main__':
     runPlugins({})
