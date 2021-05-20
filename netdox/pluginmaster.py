dd8269e382bf4abd3e95c05699ea425cca11209c
Linus Kirkwood <lkirkwood@ad.allette.com.au>
2021-05-20 17:47:03 +1000

    added support for plugins used in more complex ways than a single runner function

--- netdox/pluginmaster.py
+++ netdox/pluginmaster.py
@@ -3,7 +3,7 @@ import importlib, os
 from traceback import format_exc
 from typing import Any, Callable, Generator, Tuple
 
-def fetchRunners() -> Generator[Tuple[Callable[[dict[str, utils.DNSRecord], dict[str, utils.DNSRecord]], None], os.DirEntry, int], Any, Any]:
+def fetchPlugins() -> Generator[Tuple[Callable[[dict[str, utils.DNSRecord], dict[str, utils.DNSRecord]], None], os.DirEntry, int], Any, Any]:
     """
     Generator which yields a 3-tuple of a plugins main function, the location of said plugin, and a integer value used to decide execution order.
     """
@@ -15,14 +15,11 @@ def fetchRunners() -> Generator[Tuple[Callable[[dict[str, utils.DNSRecord], dict
             except Exception:
                 raise ImportError(f'[ERROR][plugins] Failed to import plugin {pluginName}: \n{format_exc()}')
             else:
-                if hasattr(plugin, 'runner'):
-                    if hasattr(plugin, 'stage'):
-                        stage = plugin.stage
-                    else:
-                        stage = 99
-                    yield plugin.runner, plugindir, stage
+                if hasattr(plugin, 'stage'):
+                    stage = plugin.stage
                 else:
-                    print(f'[WARNING][pluginmaster] Unable to load plugin {pluginName}: No runner found.')
+                    stage = 'none'
+                yield plugin, plugindir, stage
 
 @utils.critical
 def initPlugins():
@@ -31,28 +28,27 @@ def initPlugins():
     """
     global pluginmap
     pluginmap = {}
-    for runner, plugindir, stage in fetchRunners():
+    for plugin, plugindir, stage in fetchPlugins():
         if stage not in pluginmap:
             pluginmap[stage] = {}
-        pluginmap[stage][plugindir.name] = runner
+        pluginmap[stage][plugindir.name] = plugin
     pluginmap = {k: pluginmap[k] for k in sorted(pluginmap)}
 
 
 @utils.critical
-def runPlugins(forward_dns: dict[str, utils.DNSRecord], reverse_dns: dict[str, utils.DNSRecord]):
+def runStage(stage: str, forward_dns: dict[str, utils.DNSRecord], reverse_dns: dict[str, utils.DNSRecord]):
     """
-    Runs all initialised plugins in order.
+    Runs all initialised plugins in a given stage.
     """
     global pluginmap
-    for index, stage in pluginmap.items():
-        for plugin, runner in stage.items():
-            print(f'[INFO][pluginmaster][stage {index}] Running plugin {plugin}')
-            try:
-                runner(forward_dns, reverse_dns)
-            except Exception:
-                print(f'[ERROR][pluginmaster][stage {index}] Running {plugin} threw an exception: \n{format_exc()}')
-            else:
-                print(f'[INFO][pluginmaster][stage {index}] Plugin {plugin} completed successfully')
+    for pluginName, plugin in pluginmap[stage].items():
+        print(f'[INFO][pluginmaster][{stage}] Running plugin {pluginName}')
+        try:
+            plugin.runner(forward_dns, reverse_dns)
+        except Exception:
+            print(f'[ERROR][pluginmaster][{stage}] Running {pluginName} threw an exception: \n{format_exc()}')
+        else:
+            print(f'[INFO][pluginmaster][{stage}] Plugin {pluginName} completed successfully')
 
 if __name__ == '__main__':
     initPlugins()
