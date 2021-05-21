72c380e85a74c82212b2b2fb5b7f3695faa3f74c
Linus Kirkwood <lkirkwood@ad.allette.com.au>
2021-05-21 11:23:23 +1000

    added some init

--- netdox/plugins/dnsmadeeasy/__init__.py
+++ netdox/plugins/dnsmadeeasy/__init__.py
@@ -1,6 +1,6 @@
 from plugins.dnsmadeeasy.dnsme_api import fetchDNS as runner
 from plugins.dnsmadeeasy.dnsme_api import fetchDomains
-import json
+import json, os
 stage = 'dns'
 
 
@@ -14,5 +14,7 @@ zones = {
 for id, domain in fetchDomains():
     zones['dnsme'][domain] = id
 
+if not os.path.exists('plugins/dnsmadeeasy/src'):
+    os.mkdir('plugins/dnsmadeeasy/src')
 with open('plugins/dnsmadeeasy/src/zones.json', 'w') as stream:
     stream.write(json.dumps(zones, indent=2))
