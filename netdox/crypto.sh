18a4bb0bf9d70f18b5dba3b89ef9bed48fc9a249
Linus Kirkwood <lkirkwood@ad.allette.com.au>
2021-06-18 11:51:26 +1000

    changed logs

--- netdox/crypto.sh
+++ netdox/crypto.sh
@@ -8,16 +8,16 @@ methods=( "encrypt" "decrypt" )
 
 # input validation
 if [[ ! ${methods[@]} =~ $method ]]; then
-    echo "[ERROR][crypto.sh] $method is not a valid method."
+    echo "[ERROR][crypto] $method is not a valid method."
     exit 1
 elif [[ ! $src =~ ^(/?[a-zA-Z0-9._-]+)+$ ]]; then
-    echo "[ERROR][crypto.sh] $src is not a valid source."
+    echo "[ERROR][crypto] $src is not a valid source."
     exit 1
 elif [[ ! $dst =~ ^(/?[a-zA-Z0-9._-]+)+$ ]]; then
-    echo "[ERROR][crypto.sh] $dst is not a valid destination."
+    echo "[ERROR][crypto] $dst is not a valid destination."
     exit 1
 elif [[ ! -f $src ]]; then
-    echo "[ERROR][crypto.sh] $src is not a file."
+    echo "[ERROR][crypto] $src is not a file."
     exit 1
 fi
 
