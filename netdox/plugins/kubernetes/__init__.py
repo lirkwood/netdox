17dd82f731efe04b1c1017c2bf27a5e3fac0543f
Linus Kirkwood <lkirkwood@ad.allette.com.au>
2021-06-21 15:51:03 +1000

    moved init into a function

--- netdox/plugins/kubernetes/__init__.py
+++ netdox/plugins/kubernetes/__init__.py
@@ -22,52 +22,53 @@ from plugins.kubernetes.webhooks import app_action as k8s_app
 
 ## Initialisation
 
-# Create output dir
-for dir in ('out', 'src'):
-    if not os.path.exists(f'plugins/kubernetes/{dir}'):
-        os.mkdir(f'plugins/kubernetes/{dir}')
+def init():
+  # Create output dir
+  for dir in ('out', 'src'):
+      if not os.path.exists(f'plugins/kubernetes/{dir}'):
+          os.mkdir(f'plugins/kubernetes/{dir}')
 
-auth = utils.auth()['plugins']['kubernetes']
-with open('plugins/kubernetes/src/kubeconfig', 'w') as stream:
-    clusters = ''
-    users = ''
-    contexts = ''
-    for context in auth:
-        clusters += f"""
-        - cluster:
-            server: {auth[context]['server']}/k8s/clusters/{auth[context]['clusterId']}
-          name: {context}"""
+  auth = utils.auth()['plugins']['kubernetes']
+  with open('plugins/kubernetes/src/kubeconfig', 'w') as stream:
+      clusters = ''
+      users = ''
+      contexts = ''
+      for context in auth:
+          clusters += f"""
+          - cluster:
+              server: {auth[context]['server']}/k8s/clusters/{auth[context]['clusterId']}
+            name: {context}"""
 
-        users += f"""
-        - name: {context}
-          user:
-            token: {auth[context]['token']}
-        """
+          users += f"""
+          - name: {context}
+            user:
+              token: {auth[context]['token']}
+          """
 
-        contexts += f"""
-        - context:
-            cluster: {context}
-            user: {context}
-          name: {context}
-        """
+          contexts += f"""
+          - context:
+              cluster: {context}
+              user: {context}
+            name: {context}
+          """
 
-        current = context
+          current = context
 
-        stream.write(dedent(f"""
-        apiVersion: v1
-        Kind: Config
-        current-context: {current}
-        preferences: {{}}
-        clusters: {clusters}
-        users: {users}
-        contexts: {contexts}
-        """))
+          stream.write(dedent(f"""
+          apiVersion: v1
+          Kind: Config
+          current-context: {current}
+          preferences: {{}}
+          clusters: {clusters}
+          users: {users}
+          contexts: {contexts}
+          """))
 
-for type in ('workers', 'apps'):
-    with open(f'plugins/kubernetes/src/{type}.xml', 'w') as stream:
-        stream.write(dedent(f"""
-        <?xml version="1.0" encoding="UTF-8"?>
-        <!DOCTYPE {type} [
-        <!ENTITY json SYSTEM "{type}.json">
-        ]>
-        <{type}>&json;</{type}>""").strip())
+  for type in ('workers', 'apps'):
+      with open(f'plugins/kubernetes/src/{type}.xml', 'w') as stream:
+          stream.write(dedent(f"""
+          <?xml version="1.0" encoding="UTF-8"?>
+          <!DOCTYPE {type} [
+          <!ENTITY json SYSTEM "{type}.json">
+          ]>
+          <{type}>&json;</{type}>""").strip())
