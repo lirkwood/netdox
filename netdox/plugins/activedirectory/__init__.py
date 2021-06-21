ceae450c3d0a4de2005ea7f18f931c17573d13b3
Linus Kirkwood <lkirkwood@ad.allette.com.au>
2021-06-21 15:48:23 +1000

    moved some functions into a separate script

--- netdox/plugins/activedirectory/__init__.py
+++ netdox/plugins/activedirectory/__init__.py
@@ -1,9 +1,9 @@
 ## Runner
-from plugins.activedirectory.ad_api import fetchDNS as runner
+from plugins.activedirectory.fetch import fetchDNS as runner
 stage = 'dns'
 
 ## DNS Actions
-from plugins.activedirectory.ad_api import create_forward, create_reverse as create_PTR
+from plugins.activedirectory.create import create_forward, create_reverse as create_PTR
 
 def create_A(name: str, ip: str, zone: str):
     create_forward(name, ip, zone, 'A')
--- netdox/plugins/activedirectory/fetch.py
+++ netdox/plugins/activedirectory/fetch.py
@@ -1,4 +1,4 @@
-import os, re, json, subprocess
+import os, json
 import iptools, utils
 
 def fetchDNS(forward: dict[str, utils.DNSRecord], reverse: dict[str, utils.DNSRecord]):
@@ -107,62 +107,3 @@ def assemble_fqdn(subdomain: str, root: str) -> str:
     else:
         fqdn = subdomain + '.' + root
     return fqdn.lower()
-
-
-def create_forward(name: str, ip: str, zone: str, type: str):
-    """
-    Schedules a DNS record for creation in ActiveDirectory
-    """
-    if re.fullmatch(utils.dns_name_pattern, name) and iptools.valid_ip(ip):
-        with open('src/forward.json') as stream:
-            dns = utils.DNSSet.from_json(stream.read())
-        if (ip, 'ActiveDirectory') in dns[name]._ips:
-            return None
-
-        try:
-            subprocess.check_call('./crypto.sh decrypt plugins/activedirectory/nfs/vector.txt plugins/activedirectory/nfs/scheduled.bin plugins/activedirectory/src/scheduled.json', shell=True)
-            with open('plugins/activedirectory/src/scheduled.json', 'r') as stream:
-                existing = json.load(stream)
-        except subprocess.CalledProcessError:
-            existing = []
-        finally:
-            new = {
-                "name": name,
-                "value": ip,
-                "zone": zone,
-                "type": type
-            }
-            existing.append(new)
-            with open('plugins/activedirectory/src/scheduled.json', 'w') as stream:
-                stream.write(json.dumps(existing))
-            # subprocess.run('./crypto.sh encrypt plugins/activedirectory/nfs/vector.txt plugins/activedirectory/src/scheduled.json plugins/activedirectory/nfs/scheduled.bin', shell=True)
-
-def create_reverse(ip: str, value: str):
-    """
-    Schedules a PTR record for creation in ActiveDirectory
-    """
-    if iptools.valid_ip(ip) and re.fullmatch(utils.dns_name_pattern, value):
-        with open('src/reverse.json', 'r') as dnsstream:
-            dns = utils.DNSSet.from_json(dnsstream.read())
-            if (value, 'ActiveDirectory') in dns[ip]._ptr:
-                return None
-    
-        addr = ip.split('.')[-1]
-        zone = f'{".".join(ip.split(".")[-2::-1])}.in-addr.arpa'
-        try:
-            subprocess.check_call('./crypto.sh decrypt plugins/activedirectory/nfs/vector.txt plugins/activedirectory/nfs/scheduled.bin plugins/activedirectory/src/scheduled.json', shell=True)
-            with open('plugins/activedirectory/src/scheduled.json', 'r') as stream:
-                existing = json.load(stream)
-        except subprocess.CalledProcessError:
-            existing = []
-        finally:
-            new = {
-                "name": addr,
-                "value": value,
-                "zone": zone,
-                "type": "PTR"
-            }
-            existing.append(new)
-            with open('plugins/activedirectory/src/scheduled.json', 'w') as stream:
-                stream.write(json.dumps(existing))
-            # subprocess.run('./crypto.sh encrypt plugins/activedirectory/nfs/vector.txt plugins/activedirectory/src/scheduled.json plugins/activedirectory/nfs/scheduled.bin', shell=True)
