d75b87509c4cacbf9704bb2580e66362e73f070b
Linus <lkirkwood@allette.com.au>
2021-07-02 23:38:08 +1000

    added nat plugin subclass

--- netdox/plugins/nat/__init__.py
+++ netdox/plugins/nat/__init__.py
@@ -2,4 +2,15 @@
 Used to retrieve NAT information from FortiGate and pfSense
 """
 from plugins.nat.fetch import runner
-stage = 'dns'
+from plugins import Plugin as BasePlugin
+from networkobjs import Network
+
+class Plugin(BasePlugin):
+    name = 'nat'
+    stage = 'dns'
+
+    def init(self) -> None:
+        pass
+
+    def runner(self, network: Network) -> None:
+        runner(network)
