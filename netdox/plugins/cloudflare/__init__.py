bc5ecea9f01db5cbec4ff413d4ee9224f413f376
Linus <lkirkwood@allette.com.au>
2021-07-02 23:36:14 +1000

    added cloudflare plugin subclass

--- netdox/plugins/cloudflare/__init__.py
+++ netdox/plugins/cloudflare/__init__.py
@@ -1,5 +1,17 @@
 """
 Used to read and modify DNS records stored in CloudFlare.
 """
-from plugins.cloudflare.fetch import main as runner
-stage = 'dns'
+from networkobjs import Network
+from plugins import Plugin as BasePlugin
+from plugins.cloudflare.fetch import main
+
+
+class Plugin(BasePlugin):
+    name = 'cloudflare'
+    stage = 'dns'
+
+    def init(self) -> None:
+        pass
+
+    def runner(self, network: Network) -> None:
+        main(network)
