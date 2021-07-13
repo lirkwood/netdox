6209a9180a1410548e76cb00eb83a3132d71619b
Linus Kirkwood <lkirkwood@ad.allette.com.au>
2021-07-13 16:13:48 +1000

    removed support for init.sh in plugins

--- netdox/plugins/activedirectory/__init__.py
+++ netdox/plugins/activedirectory/__init__.py
@@ -3,6 +3,10 @@ Used to read and modify DNS records stored in ActiveDirectory.
 
 This plugin uses a shared storage location in order to pass information back and forth between the Netdox host and the ActiveDirectory DNS server.
 """
+import os
+import subprocess
+from shutil import rmtree
+
 from networkobjs import Network
 from plugins import Plugin as BasePlugin
 from plugins.activedirectory.create import create_forward, create_reverse
@@ -14,7 +18,19 @@ class Plugin(BasePlugin):
     stages = ['dns']
 
     def init(self) -> None:
-        pass
+        if os.path.exists('plugins/activedirectory/records'):
+            rmtree('plugins/activedirectory/records')
+        os.mkdir('plugins/activedirectory/records')
+
+        for file in os.scandir('plugins/activedirectory/nfs'):
+            if file.name.endswith('.bin'):
+                subprocess.run(args = [
+                    'crypto.sh',                                                            # executable
+                    'decrypt',                                                              # method
+                    'plugins/activedirectory/nfs/vector.txt',                               # IV path
+                    file.path,                                                              # input path
+                    f'plugins/activedirectory/records/{file.name.replace(".bin", ".json")}' # output path
+                ])
 
     def runner(self, network: Network, *_) -> None:
         fetchDNS(network)
--- netdox/plugins/activedirectory/init.sh
+++
@@ -1,5 +0,0 @@
-mkdir plugins/activedirectory/records
-# decrypt files
-for file in plugins/activedirectory/nfs/*.bin; do
-    ./crypto.sh decrypt 'plugins/activedirectory/nfs/vector.txt' "$file" "plugins/activedirectory/records/$(basename ${file%.bin}).json" &>/dev/null
-done
