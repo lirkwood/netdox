7557d230329f28a5cb86f6b345182f7b4fa21b88
Linus Kirkwood <lkirkwood@ad.allette.com.au>
2021-07-14 13:47:31 +1000

    added __init__.py

---
+++ netdox/__init__.py
@@ -0,0 +1,9 @@
+"""
+This module contains the public code for the Netdox project, available at https://https://gitlab.allette.com.au/allette/devops/network-documentation/.
+"""
+
+import networkobjs
+import pageseeder
+import plugins
+import iptools
+import utils
