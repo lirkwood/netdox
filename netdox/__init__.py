7ffdbb6e3c8a302b56fa737b2184fc648b68c4cc
Linus Kirkwood <lkirkwood@ad.allette.com.au>
2021-07-15 11:56:46 +1000

    removed __init__ for non package branch

--- netdox/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-This module contains the public code for the Netdox project, available at https://https://gitlab.allette.com.au/allette/devops/network-documentation/.
-"""
-
-import networkobjs
-import pageseeder
-import plugins
-import iptools
-import utils
