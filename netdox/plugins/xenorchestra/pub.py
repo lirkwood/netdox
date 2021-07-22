9868aad2301845b52a1751b3160e1e338bac8dbd
Linus Kirkwood <lkirkwood@ad.allette.com.au>
2021-07-22 17:32:26 +1000

    added pub module for generating xopub

---
+++ netdox/plugins/xenorchestra/pub.py
@@ -0,0 +1,58 @@
+from bs4 import BeautifulSoup
+from networkobjs import Network
+
+def genpub(network: Network, vms: dict, hostVMs: dict, poolHosts: dict) -> None:
+    """
+    Generates a publication linking pools to hosts to vms
+
+    :param network: The network
+    :type network: Network
+    :param vms: A dictionary mapping VM UUIDs to a dict of details
+    :type vms: dict
+    :param hostVMs: A dictionary mapping Host IP addresses to VM UUIDs
+    :type hostVMs: dict
+    :param poolHosts: A dictionary mapping pool names to host IP addresses.
+    :type poolHosts: dict
+    """
+    pub = BeautifulSoup(TEMPLATE, features='xml')
+    section = pub.find('section', id = 'pools')
+    count = 0
+    for pool, hostiplist in poolHosts.items():
+        heading = pub.new_tag('heading', level = 2)
+        heading.string = pool
+        section.append(heading)
+        heading.wrap(pub.new_tag('fragment', id = f'heading_{count}'))
+
+        xfrag = pub.new_tag(name = 'xref-fragment', id = f'pool_{count}')
+
+        for hostip in hostiplist:
+            xfrag.append(pub.new_tag('blockxref', docid = network.ips[hostip].node.docid))
+
+            for vmid in hostVMs[hostip]:
+                if 'mainIpAddress' in vms[vmid]:
+                    vmip = vms[vmid]['mainIpAddress']
+                    xfrag.append(pub.new_tag('blockxref', docid = network.ips[vmip].node.docid, level = 1))
+        count += 1
+
+    with open('out/xopub.psml', 'w') as stream:
+        stream.write(pub.prettify())
+
+
+TEMPLATE = '''
+<document level="portable" type="references">
+
+    <documentinfo>
+        <uri title="Xen Orchestra Pools" />
+        <publication id="_nd_xo_pub" title="Xen Orchestra Pools" />
+    </documentinfo>
+
+    <section id="title">
+        <fragment id="title">
+            <heading level="1">Xen Orchestra Pools</heading>
+        </fragment>
+    </section>
+
+    <section id="pools" />
+    
+</document>
+'''
