1a71010afa53452bc43bc9978de967dff990516d
Linus Kirkwood <lkirkwood@ad.allette.com.au>
2021-07-26 15:13:00 +1000

    reworked the zip_upload and added clear_loading_zone

--- netdox/pageseeder.py
+++ netdox/pageseeder.py
@@ -371,10 +371,30 @@ def load_loading_zone(params={}, host='', group='', member='', header={}):
 
 
 @auth
+def clear_loading_zone(params={}, host='', group='', member='', header={}):
+    service = f'/members/{member}/groups/{group}/loadingzone/clear'
+    r = requests.post(host+service, headers=header, params=params)
+    return r.text
+
+
+@auth
 def zip_upload(path, uploadpath, host='', group='', header={}):
     loading_zone_upload(path, params={'file':'netdox-psml.zip'}, host='https://ps-netdox.allette.com.au', group=group, header=header)
+    print('[INFO][upload] File sent successfully.')
     thread = BeautifulSoup(unzip_loading_zone('netdox-psml.zip', params={'deleteoriginal':'true'}), features = 'xml').thread
     while thread['status'] != 'completed':
         sleep(2)
-        thread = BeautifulSoup(get_thread(thread['id']), features = 'xml').thread
-    return load_loading_zone(params={'folder': uploadpath})
+        try:
+            thread = BeautifulSoup(get_thread(thread['id']), features = 'xml').thread
+        except TypeError:
+            print('[ERROR][upload] Upload failed. Clearing loading zone...')
+            clear_loading_zone()
+            return
+
+    print('[INFO][upload] File unzipped. Loading files into PageSeeder.')
+    return load_loading_zone(params={
+        'folder': uploadpath,
+        'overwrite': 'true',
+        'overwrite-properties': 'true',
+        'validate': 'false'
+        })
