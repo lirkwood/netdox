248f7589211541f7b1db968925e584099d5e8616
Linus Kirkwood <lkirkwood@ad.allette.com.au>
2021-07-29 10:52:31 +1000

    finished implementing pfsense scraper in pyppeteer

--- netdox/plugins/nat/fetch.py
+++ netdox/plugins/nat/fetch.py
@@ -40,7 +40,21 @@ def runner(network: Network):
 
 
 async def pfsenseScrapeNat() -> dict:
+    nat = {}
     browser = await launch(args = ['--no-sandbox'])
     page = await browser.newPage()
-    gateway = 'https://'.concat(utils.config()['nat']['host'])
+    gateway = f"https://{utils.config()['plugins']['nat']['host']}/"
     await page.goto(gateway, waitUntil = 'networkidle0')
+
+    await (await page.J('#usernamefld')).type(utils.config()['plugins']['nat']['username'])
+    await (await page.J('#passwordfld')).type(utils.config()['plugins']['nat']['password'])
+    await page.click('.btn-sm')
+
+    await page.goto(f"https://{utils.config()['plugins']['nat']['host']}/firewall_nat_1to1.php")
+    if await page.Jeval('.panel-title', 'title => title.textContent') == 'NAT 1:1 Mappings':
+        rows = await page.JJ('tr.ui-sortable-handle')
+        for row in rows:
+            columns = await row.JJeval('td', 'columns => columns.map(column => column.textContent.trim())')
+            nat[columns[3]] = columns[4]
+            nat[columns[4]] = columns[3]
+    return nat
