374366b313d2d01778843bd47a3ff89a36f302a2
Linus Kirkwood <lkirkwood@ad.allette.com.au>
2021-08-04 10:26:34 +1000

    switched to new crypto

--- netdox/plugins/activedirectory/__init__.py
+++ netdox/plugins/activedirectory/__init__.py
@@ -4,7 +4,7 @@ Used to read and modify DNS records stored in ActiveDirectory.
 This plugin uses a shared storage location in order to pass information back and forth between the Netdox host and the ActiveDirectory DNS server.
 """
 import os
-import subprocess
+import crypto
 from shutil import rmtree
 
 from networkobjs import Network
@@ -24,16 +24,11 @@ class Plugin(BasePlugin):
 
         for file in os.scandir('plugins/activedirectory/src'):
             if file.name.endswith('.bin'):
-                subprocess.run(args = [
-                    './crypto.sh',                                                          # executable
-                    'decrypt',                                                              # method
-                    'plugins/activedirectory/src/vector.txt',                               # IV path
-                    file.path,                                                              # input path
-                    f'plugins/activedirectory/records/{file.name.replace(".bin", ".json")}' # output path
-                ])
+                crypto.decrypt_file(file.path, 'plugins/activedirectory/records/'+ file.name.replace('.bin', '.json'))
 
     def runner(self, network: Network, *_) -> None:
         fetchDNS(network)
+        rmtree('plugins/activedirectory/records')
 
     def create_A(self, name: str, ip: str, zone: str):
         create_forward(name, ip, zone, 'A')
