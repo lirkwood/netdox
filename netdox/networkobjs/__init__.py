65c9b470f4d1becf195762d3ab2d2dbf69ca69f0
Linus Kirkwood <lkirkwood@ad.allette.com.au>
2021-08-09 13:34:20 +1000

    updated docs

--- netdox/networkobjs/__init__.py
+++ netdox/networkobjs/__init__.py
@@ -1,14 +1,12 @@
 """
 This module contains the NetworkObject classes used to store the DNS and Node information, their containers, and two helper classes.
 
-The NetworkObject class is an abstract base class subclassed by Domain, IPv4Address, and Node. 
-NetworkObjects represent one type of object in the network. 
-It could be a unique FQDN found in a managed DNS zone, an IP address one of those domains resolves to, or a Node.
+The NetworkObject class is an abstract base class representing one instance of one type of object in the network. 
+All NetworkObjects currently implemented can be further categorised into DNSObjects and Nodes.
+DNSObjects represent the endpoints for DNS records: either a FQDN (Domain) or an IPv4 address.
 A Node is representative of a single machine / virtualised machine.
-When writing plugins for the node stage developers are encouraged to write their own subclass of Node, 
-specific to the target of their plugin.
-This allows you to define how the Node will behave when it is added to a NodeSet or Network, 
-and it's strategy for merging with other Nodes.
+DefaultNode is an implementation of Node which has exactly one private IPv4 address (and any number of public IPv4s).
+This may be useful for plugin writers who do not want to implement the whole Node class.
 """
 
 import base, containers, helpers
