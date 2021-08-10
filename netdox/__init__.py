3c838bd205c4fd22f2b272c17bf6114ccbe20e9d
Linus Kirkwood <lkirkwood@ad.allette.com.au>
2021-08-10 16:48:00 +1000

    added __init__

---
+++ netdox/__init__.py
@@ -0,0 +1,11 @@
+"""
+Netdox is a network visualisation tool. 
+This package contains the source code of that tool, and can be used to develop plugins that extend the functionality of Netdox.
+"""
+
+import networkobjs
+import crypto
+import iptools
+import pageseeder
+import utils
+import psml
