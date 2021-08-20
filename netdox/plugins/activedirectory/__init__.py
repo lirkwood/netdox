0e1e1e52daba9351e91ae531ac99e353dd198df5
Linus Kirkwood <lkirkwood@allette.com.au>
2021-08-20 19:08:59 +1000

    added footers hook

--- netdox/plugins/activedirectory/__init__.py
+++ netdox/plugins/activedirectory/__init__.py
@@ -4,6 +4,10 @@ Used to read and modify DNS records stored in ActiveDirectory.
 This plugin uses a shared storage location in order to pass information back and forth between the Netdox host and the ActiveDirectory DNS server.
 """
 
-from netdox.plugins.activedirectory.dns import fetchDNS as dns_runner
+from netdox.plugins.activedirectory.dns import fetchDNS
+from netdox.plugins.activedirectory.footers import addFooters
 
-__stages__ = {'dns': dns_runner}
+__stages__ = {
+    'dns': fetchDNS,
+    'footers': addFooters
+}
