789ae23e58ce1059479249de034228fbaeae0a19
Linus Kirkwood <lkirkwood@allette.com.au>
2021-08-20 12:55:28 +1000

    reworked screenshots to be simpler / more readable and fixed bugs

--- netdox/plugins/screenshots/__init__.py
+++ netdox/plugins/screenshots/__init__.py
@@ -8,6 +8,7 @@ from typing import Iterable, Tuple
 import diffimg
 from bs4 import BeautifulSoup
 from pyppeteer import launch
+from pyppeteer.browser import Browser
 from pyppeteer.page import Page
 
 from netdox import pageseeder, utils
@@ -15,20 +16,31 @@ from netdox.objs import Domain, Network
 
 
 class ScreenshotManager:
-    domains: list[str]
     workdir: str
+    """Directory to save temporary files to."""
     basedir: str
+    """Directory of base images to diff against."""
     outdir: str
+    """Directory to save output images to."""
     placeholder: str
+    """Path to a placeholder image."""
+    existingScreens: list[str]
+    """List of filenames of screenshots on PageSeeder."""
     roles: list[str]
-    failed: list[str]
-    diff: list[str]
-    nobase: list[str]
+    """List of roles that have the `screenshot` property set."""
+    _failed: list[str]
+    """List of domain docids that failed to be screenshot"""
+    _diff: list[str]
+    """List of domain docids that had a screenshot >5% different to the base img"""
+    _noBase: list[str]
+    """List of domain docids that had no base img to diff against"""
 
     def __init__(self, domains: Iterable[Domain], workdir: str, basedir: str, outdir: str, placeholder: str) -> None:
-        self.failed = []
-        self.diff = []
-        self.nobase = []
+        self.stats = {
+            'fail': [],
+            'diff': [],
+            'base': []
+        }
 
         self.workdir = workdir
         self.basedir = basedir
@@ -48,71 +60,86 @@ class ScreenshotManager:
 
     def start(self) -> None:
         """
-        Screenshots all domains with the correct role config, and then compares them to an existing screenshot.
+        Screenshots all domains with the `screenshot` property set in the domain's role, 
+        and then diffs them to against a base image (previous screenshot).
 
-        If the domain cannot be screenshotted, and there is not a screenshot on PageSeeder already, a placeholder is generated.
-        If the screenshot is different to the existing screenshot, 
-        the existing file is copied to a dated archive directory and the new one will be uploaded.
+        If screenshot fails, and there is not a screenshot on PageSeeder already, a placeholder is generated.
+        If the screenshot is different to the base image (by >5%), 
+        the base is copied to a dated archive directory and uploaded as well for human inspection.
         """
-        if os.path.exists(self.workdir+'/ss'):
-            shutil.rmtree(self.workdir+'/ss')
-        os.mkdir(self.workdir+'/ss')
-
         for domain, success in self.takeScreens():
-            # if no screen
+            filename = domain.docid + '.jpg'
             if not success:
-                self.failed.append(domain.docid)
-                # copy placeholder
-                if f'{domain.docid}.jpg' not in self.existingScreens:
-                    shutil.copyfile(
-                        self.placeholder, 
-                        f'{self.outdir}/screenshots/{domain.docid}.jpg'
-                    )
-
+                self.stats['fail'].append(domain.docid)
+                self.copyPlaceholder(filename)
             else:
                 domain.psmlFooter.append(BeautifulSoup(f'''
                     <fragment id="screenshot">
                         <para><image src="{utils.config()["pageseeder"]["group"].replace("-","/")}/website/screenshots/{domain.docid}.jpg"
                     </fragment>
                 ''', features = 'xml'))
-                
-                try:
-                    diffratio = diffimg.diff(
-                        im1_file = f'{self.basedir}/{domain.docid}.jpg', 
-                        im2_file = f'{self.workdir}/ss/{domain.docid}.jpg',
-                        diff_img_file = f'{self.outdir}/diffimg/{domain.docid}.jpg'
-                    )
-                except FileNotFoundError:
-                    # if no base img
-                    self.nobase.append(domain.docid)
-                    shutil.copyfile(
-                        f'{self.workdir}/ss/{domain.docid}.jpg',
-                        f'{self.outdir}/screenshots/{domain.docid}.jpg'
-                    )
-                else:
-                    # if diff > 5%
-                    if diffratio > 0.05:
-                        self.diff.append(domain.docid)
-                        # save old base img
-                        shutil.copyfile(
-                            f'{self.basedir}/{domain.docid}.jpg', 
-                            f'{self.outdir}/screenshot_history/{date.today().isoformat()}/{domain.docid}.jpg'
-                        )
-                        # copy new screen
-                        shutil.copyfile(
-                            f'{self.workdir}/ss/{domain.docid}.jpg',
-                            f'{self.outdir}/screenshots/{domain.docid}.jpg'
-                        )
-                        shutil.copyfile(
-                            f'{self.workdir}/screenshots/{domain.docid}.jpg',
-                            f'{self.basedir}/{domain.docid}.jpg'
-                        )
+
+                if os.path.exists(f'{self.basedir}/{filename}'):
+
+                    if diffimg.diff(
+                        im1_file = f'{self.basedir}/{filename}', 
+                        im2_file = f'{self.workdir}/{filename}',
+                        diff_img_file = f'{self.outdir}/diffimg/{filename}'
+                    ) > 0.05:
+                        # if diff > 5%
+                        self.stats['diff'].append(domain.docid)
+                        self.newBase(filename, archive = True)
+
                     else:
-                        # delete empty diff file
-                        os.remove(f'{self.outdir}/diffimg/{domain.docid}.jpg')
-        
+                        os.remove(f'{self.outdir}/diffimg/{filename}')
+
+                else:
+                    self.stats['base'].append(domain.docid)
+                    self.newBase(filename)
+
         with open(f'{self.workdir}/stats.json', 'w') as stream:
             stream.write(json.dumps(self.stats))
+    
+    ## Manipulating screenshot files
+
+    def newBase(self, filename: str, archive = False) -> None:
+        """
+        Copies the new screenshot to the output and base dirs.
+        If *archive* is True, copies the old base image to screenshot_history.
+
+        :param filename: Filename of the screenshot in question.
+        :type filename: str
+        :param archive: Whether or not to archive the old base image, defaults to False.
+        :type archive: bool, optional
+        """
+        if archive:
+            shutil.copyfile(
+                f'{self.basedir}/{filename}', 
+                f'{self.outdir}/screenshot_history/{date.today().isoformat()}/{filename}'
+            )
+        shutil.copyfile(
+            f'{self.workdir}/{filename}',
+            f'{self.outdir}/screenshots/{filename}'
+        )
+        shutil.copyfile(
+            f'{self.workdir}/{filename}',
+            f'{self.basedir}/{filename}'
+        )
+
+    def copyPlaceholder(self, filename: str) -> None:
+        """
+        Copies placeholder if no image on PS already.
+
+        :param filename: Filename of the screenshot in question.
+        :type filename: str
+        """
+        if f'{filename}.jpg' not in self.existingScreens:
+            shutil.copyfile(
+                self.placeholder, 
+                f'{self.outdir}/screenshots/{filename}.jpg'
+            )
+
+    ## Taking screenshots
 
     def takeScreens(self) -> list[Tuple[Domain, bool]]:
         """
@@ -150,17 +177,14 @@ class ScreenshotManager:
         :return: A list of tuples containing a Domain object and boolean. True if successfully screenshotted.
         :rtype: list[Tuple[Domain, bool]]
         """
-        browser = await launch(args = ['--no-sandbox'])
-        page = await browser.newPage()
-        await page.setViewport({'width':1680,'height':1050})
-
+        browser = await launch(args = ['--no-sandbox'], autoClose = False)
         values = await asyncio.gather(
-            *[self.screenshot(page, domain) for domain in domains]
+            *[self.screenshot(browser, domain) for domain in domains]
         )
         await browser.close()
         return values
 
-    async def screenshot(self, page: Page, domain: Domain) -> bool:
+    async def screenshot(self, browser: Browser, domain: Domain) -> bool:
         """
         Takes a screenshot of a domain
 
@@ -171,20 +195,16 @@ class ScreenshotManager:
         :return: True if a screenshot is successfully saved. False otherwise.
         :rtype: bool
         """
+        print(domain.name)
         try:
-            await page.goto(f'https://{domain.name}/', timeout = 3000)
-            await page.screenshot(path = f'{self.workdir}/ss/{domain.docid}.jpg')
+            page = await browser.newPage()
+            await page.setViewport({'width':1680,'height':1050})
+            await page.goto(f'https://{domain.name}/', waitUntil = 'domcontentloaded', timeout = 3000)
+            await page.screenshot(path = f'{self.workdir}/{domain.docid}.jpg')
+            await page.close()
             return (domain, True)
         except Exception:
             return (domain, False)
-
-    @property
-    def stats(self) -> dict[str, list[str]]:
-        return {
-            'failed': self.failed,
-            'nobase': self.nobase,
-            'diff': self.diff
-        }
 
 
 def init() -> None:
@@ -192,16 +212,19 @@ def init() -> None:
         shutil.rmtree(utils.APPDIR+ 'plugins/screenshots/src')
     if os.path.exists(utils.APPDIR+ 'out/screenshots'):
         shutil.rmtree(utils.APPDIR+ 'out/screenshots')
+    if os.path.exists(utils.APPDIR+ 'out/diffimg'):
+        shutil.rmtree(utils.APPDIR+ 'out/diffimg')
     if os.path.exists(utils.APPDIR+ 'out/screenshot_history/'+ date.today().isoformat()):
         shutil.rmtree(utils.APPDIR+ 'out/screenshot_history/'+ date.today().isoformat())
 
-    if not os.path.exists(utils.APPDIR+ 'src/base'):
-        os.mkdir(utils.APPDIR+ 'src/base')
+    if not os.path.exists(utils.APPDIR+ 'plugins/screenshots/base'):
+        os.mkdir(utils.APPDIR+ 'plugins/screenshots/base')
     if not os.path.exists(utils.APPDIR+ 'out/screenshot_history'):
         os.mkdir(utils.APPDIR+ 'out/screenshot_history')
         
     os.mkdir(utils.APPDIR+ 'plugins/screenshots/src')
     os.mkdir(utils.APPDIR+ 'out/screenshots')
+    os.mkdir(utils.APPDIR+ 'out/diffimg')
     os.mkdir(utils.APPDIR+ 'out/screenshot_history/'+ date.today().isoformat())
 
 def runner(network: Network) -> None:
@@ -213,6 +236,29 @@ def runner(network: Network) -> None:
         placeholder = utils.APPDIR+ 'src/placeholder.jpg'
     )
     mngr.start()
+    return mngr.stats
 
 
 __stages__ = {'footers': runner}
+
+
+
+from pprint import pprint
+
+if __name__ == '__main__':
+
+    net = Network.fromDump()
+    domains = []
+    for domain in net.domains.roles['website']:
+        if domain in net.domains:
+            domains.append(net.domains[domain])
+
+    mngr = ScreenshotManager(
+        domains = domains[:9], 
+        workdir = utils.APPDIR+ 'plugins/screenshots/src',
+        basedir = utils.APPDIR+ 'plugins/screenshots/base',
+        outdir = utils.APPDIR+ 'out',
+        placeholder = utils.APPDIR+ 'src/placeholder.jpg'
+    )
+    mngr.start()
+    pprint(mngr.stats)
