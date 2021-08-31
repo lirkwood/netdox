d30c6b50db44e71c03bc200eaba3bb9be03e4884
Linus Kirkwood <lkirkwood@allette.com.au>
2021-08-31 10:46:30 +1000

    removed imports from __init__ to avoid duplication

--- netdox/__init__.py
+++ netdox/__init__.py
@@ -2,12 +2,3 @@
 Netdox is a network visualisation tool. 
 This package contains the source code of that tool, and can be used to develop plugins that extend the functionality of Netdox.
 """
-
-from . import (
-    objs,
-    crypto,
-    iptools,
-    pageseeder,
-    utils,
-    psml
-)
