d367c350ff875773f2c07773fd0891ac7e461621
Linus Kirkwood <lkirkwood@allette.com.au>
2021-09-20 13:07:30 +1000

    more helper tests

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,10 +1,12 @@
-from conftest import hide_file, LOCATIONS
+import pytest
+from conftest import LOCATIONS, hide_file
 from netdox import utils
-from netdox.objs import helpers
-from pytest import fixture
+from netdox.objs import helpers, nwobjs
+from test_nwobjs import network, domain, ipv4, node
+from lxml import etree
 
 
-@fixture
+@pytest.fixture
 def locator():
     return helpers.Locator()
 
@@ -15,7 +17,6 @@ class TestLocator:
         hide_file(utils.APPDIR + 'cfg/locations.json')
         assert helpers.Locator().location_map == {}
 
-
     def test_locate(self, locator: helpers.Locator):
         assert locator.locate(['192.168.0.0']) == 'Subnet0'
         assert locator.locate(['192.168.0.0', '192.168.0.255']) == 'Subnet0'
@@ -24,3 +25,44 @@ class TestLocator:
         assert locator.locate(['192.168.1.0', '192.168.2.255']) == 'Subnet1or2'
 
         assert locator.locate(['192.168.0.0', '192.168.1.0']) == None
+
+
+@pytest.fixture
+def psml_schema():
+    return etree.XMLSchema(file = utils.APPDIR+ 'src/psml.xsd')
+
+class TestPSMLWriter:
+
+    def test_serialise_domain(self, domain: nwobjs.Domain, psml_schema: etree.XMLSchema):
+        """
+        Assert that the serialise method generates valid PSML from a Domain.
+        """
+        helpers.PSMLWriter().serialise(domain)
+        assert psml_schema.validate(etree.parse(domain.outpath))
+
+    def test_serialise_ipv4(self, ipv4: nwobjs.IPv4Address, psml_schema: etree.XMLSchema):
+        """
+        Assert that the serialise method generates valid PSML from a IPv4Address.
+        """
+        helpers.PSMLWriter().serialise(ipv4)
+        assert psml_schema.validate(etree.parse(ipv4.outpath))
+
+    def test_serialise_node(self, node: nwobjs.Node, psml_schema: etree.XMLSchema):
+        """
+        Assert that the serialise method generates valid PSML from a Node.
+        """
+        helpers.PSMLWriter().serialise(node)
+        assert psml_schema.validate(etree.parse(node.outpath))
+        
+
+class TestRecordSet:
+
+    @pytest.mark.parametrize('type', ['A', 'PTR', 'CNAME', 'NAT'])
+    def test_constructor(self, type: str):
+        recordset = helpers.RecordSet(type)
+        assert recordset.record_type == helpers.RecordType[type]
+
+    def test_add(self):
+        recordset = helpers.RecordSet('CNAME')
+        recordset.add('SomE.DomaiN.com   ', 'source')
+        assert recordset.names == ['some.domain.com']
