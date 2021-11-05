8745f4b973e08f1a216b21541fb03d00df00ad3c
Linus Kirkwood <lkirkwood@allette.com.au>
2021-11-05 11:24:59 +1100

    removed role fetching from refresh init

--- src/netdox/refresh.py
+++ src/netdox/refresh.py
@@ -6,15 +6,11 @@ It runs some initialisation first, then calls the *dns* plugins, the *resource* 
 then calls the final plugin stage and writes PSML. The upload is managed by the caller executable Netdox (see :ref:`file_netdox`)
 """
 
-import json
 import logging
 import os
 import shutil
-from distutils.util import strtobool
 
-from bs4 import BeautifulSoup
-
-from netdox import pageseeder, psml, utils
+from netdox import pageseeder, utils
 from netdox.objs import NetworkManager
 
 logger = logging.getLogger(__name__)
@@ -25,10 +21,7 @@ logger = logging.getLogger(__name__)
 
 def init():
     """
-    Some initialisation to run every time the data is refreshed.
-
-    Removes any leftover output files, initialises all plugins, and loads the dns roles and other config from PageSeeder.
-    If this config is not present or Netdox is unable to find or parse it, a default set of config files is copied to the upload context.
+    Removes old, populated output directories and recreates them.
     """
     if not os.path.exists(utils.APPDIR+ 'out'):
         os.mkdir(utils.APPDIR+ 'out')
@@ -42,79 +35,10 @@ def init():
     for outfolder in ('config', 'domains', 'ips', 'nodes'):
         os.mkdir(utils.APPDIR+ 'out'+ os.sep+ outfolder)
 
-    roles = {"exclusions": []}
-    # load dns config from pageseeder
-    psConfigInf = json.loads(pageseeder.get_uri('_nd_config'))
-    if 'title' in psConfigInf and psConfigInf['title'] == 'DNS Config':
-        # load roles fragment
-        roleFrag = BeautifulSoup(pageseeder.get_fragment('_nd_config', 'roles'), features='xml')
-        for xref in roleFrag("xref"):
-            try:
-                roleUri = xref['uriid']
-                roleConfig = psml.pfrag2dict(pageseeder.get_fragment(roleUri, 'config'))
-                roleName = roleConfig['name']
-
-                # set role for configured domains
-                domains = set()
-                revXrefs = BeautifulSoup(pageseeder.get_xrefs(roleUri), features='xml')
-                for revXref in revXrefs("reversexref"):
-                    ## change to 'domain'
-                    if 'documenttype' in revXref.attrs and revXref['documenttype'] == 'domain':
-                        domains.add(revXref['urititle'])
-                
-                screenshot = strtobool(roleConfig['screenshot'])
-                del roleConfig['name'], roleConfig['screenshot']
-                
-                roles[roleName] = (roleConfig | {
-                    "screenshot": screenshot,
-                    "domains": list(domains),
-                    "uri": roleUri
-                })
-            except Exception:
-                logger.error(f'Failed to load DNS role {xref["urititle"]}')
-
-        # load exclusions
-        exclusionSoup = BeautifulSoup(pageseeder.get_fragment('_nd_config', 'exclude'), features='xml')
-        for para in exclusionSoup("para"):
-            roles['exclusions'].append(para.string)
-
-    else:
-        logger.warning('No DNS config found on PageSeeder')
-        # load default config and copy to upload context
-        for file in os.scandir(utils.APPDIR+ 'src/defaults/psconf'):
-            if file.name != 'config.psml':
-                with open(file, 'r') as stream:
-                    soup = BeautifulSoup(stream.read(), features='xml')
-                    roleConfig = psml.pfrag2dict(soup.find(id="config")) | {'domains':[]}
-                    roles[roleConfig['name']] = roleConfig
-
-            shutil.copyfile(file.path, utils.APPDIR+ f'out/config/{file.name}')
-
-    # load locally configured roles
-    local = utils.roles()
-
-    # merge local and ps configured role sets
-    for role, roleConfig in local.items():
-        if role == 'exclusions':
-            roles[role] = sorted(set(roles[role] + local[role]))
-        elif role not in roles:
-            utils.roleToPSML(role)
-            roles[role] = local[role]
-        else:
-            for attribute, value in roleConfig.items():
-                if attribute == 'domains':
-                    roles[role][attribute] = sorted(set(roles[role][attribute] + value))
-                else:
-                    roles[role][attribute] = value
-
-    with open(utils.APPDIR+ 'cfg/roles.json', 'w') as stream:
-        stream.write(json.dumps(roles, indent=2))
-
 
 def main():
     """
     The main flow of the refresh process.
-
     Calls most other functions in this script in the required order.
     """
 
