2b048d04face2e7237f04b0951da36dd0f592a69
Linus Kirkwood <lkirkwood@allette.com.au>
2021-11-15 17:21:15 +1100

    gendocs now buils psml with no toc

--- scripts/gendocs.ps1
+++ scripts/gendocs.ps1
@@ -2,6 +2,6 @@ Push-Location
 Set-Location "$PSScriptRoot\..\docs"
 Remove-Item .\_source\source\*
 Remove-Item -Recurse .\_build\apidoc\*
-sphinx-apidoc --implicit-namespaces -M -o .\_source\source\ ..\src\netdox\
-sphinx-build -Ea .\_source\ .\_build\apidoc
+sphinx-apidoc --implicit-namespaces -M -T -o .\_source\source\ ..\src\netdox\
+sphinx-build -Ea -b psml .\_source\ .\_build\apidoc
 Pop-Location
--- scripts/gendocs.sh
+++ scripts/gendocs.sh
@@ -4,6 +4,6 @@ pushd
 cd dirname "$0/../docs"
 rm -f _source/source/*
 rm -rf _build/apidoc
-sphinx-apidoc --implicit-namespaces -M -o _source/source ../src/netdox
-sphinx-build -Ea _source _build/apidoc
+sphinx-apidoc --implicit-namespaces -M -T -o _source/source ../src/netdox
+sphinx-build -Ea -b psml _source _build/apidoc
 popd
