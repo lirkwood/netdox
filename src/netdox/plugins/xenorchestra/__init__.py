eb48531a5e2b89f20b3c83401c2b3991c9ecb332
Linus Kirkwood <lkirkwood@allette.com.au>
2021-11-16 18:31:42 +1100

    added init method to xo plugin

--- src/netdox/plugins/xenorchestra/__init__.py
+++ src/netdox/plugins/xenorchestra/__init__.py
@@ -4,8 +4,10 @@ Used to read and modify the VMs managed by Xen Orchestra
 from __future__ import annotations
 
 import logging
+import os
 
 from netdox import Network
+from netdox.utils import APPDIR
 
 logging.getLogger('websockets').setLevel(logging.INFO)
 
@@ -23,7 +25,9 @@ def _write(network: Network) -> None:
     genpub(network, pubdict)
     genreport(network)
 
-
+def init():
+    if not os.path.exists(APPDIR + 'plugins/xenorchestra/src'):
+        os.mkdir(APPDIR + 'plugins/xenorchestra/src')
 
 __stages__ = {
     'nodes': nodes,
