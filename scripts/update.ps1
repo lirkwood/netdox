fcd7b17b7afbbee0520f6e4eb69c2210c1273c07
Linus Kirkwood <lkirkwood@allette.com.au>
2021-11-25 14:52:04 +1100

    made update scripts dir independent

--- scripts/update.ps1
+++ scripts/update.ps1
@@ -1,4 +1,7 @@
+Push-Location
+Set-Location "$PSScriptRoot\..\"
 Remove-Item .\dist\*.whl
 pip uninstall -y netdox
 python setup.py bdist_wheel
 Get-ChildItem .\dist\*.whl | % {pip install $_.FullName}
+Pop-Location
--- scripts/update.sh
+++ scripts/update.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+pushd
+cd dirname "$0/.."
 pip uninstall -y netdox
 rm -f dist/*.whl
 python3 setup.py bdist_wheel
 for file in dist/*.whl; do pip install $file; done
+popd
