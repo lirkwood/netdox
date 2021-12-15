3a6ec0708299cdcd5eff2a1f02f797d4f75efecd
Linus Kirkwood <lkirkwood@allette.com.au>
2021-12-15 12:42:29 +1100

    sentenceStale no longer fails when no expiration set

--- src/netdox/pageseeder.py
+++ src/netdox/pageseeder.py
@@ -199,7 +199,9 @@ def sentence_uri(uri: str) -> date:
             match = re.fullmatch(utils.expiry_date_pattern, label)
             if match:
                 return date.fromisoformat(match['date'])
-        raise RuntimeError(f'URI {uri} is marked stale but has no expiry date.')
+        labels.append(f'expires-{plus_thirty}')
+        patch_uri(uri, {'labels':','.join(labels)})
+        return plus_thirty
 
 def clear_sentence(uri: str) -> None:
     """
