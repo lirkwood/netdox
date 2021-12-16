f1ab185daac785fdf3a9a436ae90a9a1a952b59c
Linus Kirkwood <lkirkwood@allette.com.au>
2021-12-16 12:36:22 +1100

    added more tests

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -3,6 +3,8 @@ from conftest import LOCATIONS, hide_file
 from netdox import utils
 from netdox import helpers
 from fixtures import network, domain, ipv4, node
+from os import remove
+from lxml import etree
 
 class TestLocator:
     
@@ -19,7 +21,42 @@ class TestLocator:
         assert locator.locate(['192.168.1.0']) == 'Subnet1or2'
         assert locator.locate(['192.168.1.0', '192.168.2.255']) == 'Subnet1or2'
 
-        assert locator.locate(['192.168.0.0', '192.168.1.0']) == None    
+        assert locator.locate(['192.168.0.0', '192.168.1.0']) == None   
+
+class TestReport:
+    SECTION_ID = 'section_id'
+    OUTPATH = 'test_report.psml'
+
+    @pytest.fixture
+    def valid_section(self) -> str:
+        return f'<section id="{self.SECTION_ID}" />'
+
+    @pytest.fixture
+    def invalid_section(self) -> str:
+        return '<section><random_tag!></section>'
+
+    def test_add_section_success(self, valid_section):
+        report = helpers.Report()
+        report.addSection(valid_section)
+        assert report.sections == [valid_section]
+
+    def test_add_section_failure(self, invalid_section):
+        report = helpers.Report()
+        report.addSection(invalid_section)
+        assert report.sections == []
+
+    def test_write_report(self, valid_section):
+        report = helpers.Report()
+        report.addSection(valid_section)
+        report.writeReport(self.OUTPATH)
+
+        try:
+            etree.XMLSchema(file = utils.APPDIR + 'src/psml.xsd').assertValid(
+                etree.parse(self.OUTPATH))
+        except Exception:
+            assert False, 'Invalid report content.'
+        finally:
+            remove(self.OUTPATH)
 
 class TestOrganization:
     NAME = 'organization Name'
@@ -29,13 +66,13 @@ class TestOrganization:
     CONTACT_PHONE = '123456789'
 
     @pytest.fixture
-    def organization(self):
+    def organization(self) -> helpers.Organization:
         return helpers.Organization(
             self.NAME, self.URIID, self.CONTACT_NAME, 
             self.CONTACT_EMAIL, self.CONTACT_PHONE
         )
 
-    def test_from_psml(self, organization):
+    def test_from_psml_success(self, organization):
         assert organization == helpers.Organization.from_psml(
             f'''<document id="{self.URIID}">
                 <section id="title">
@@ -48,6 +85,23 @@ class TestOrganization:
                         <property name="admin-name" title="Admin contact name" value="{self.CONTACT_NAME}"/>
                         <property name="admin-email" title="Admin contact email" value="{self.CONTACT_EMAIL}"/>
                         <property name="admin-phone" title="Admin contact phone" value="{self.CONTACT_PHONE}"/>
+                    </properties-fragment>
+                </section>
+            </document>''')
+
+    def test_from_psml_failure(self):
+        with pytest.raises(ValueError):
+            helpers.Organization.from_psml(f'''<document id="{self.URIID}">
+                <section id="title">
+                    <fragment xmlns:psof="http://www.pageseeder.org/function" id="1">
+                        <heading level="1">{self.NAME}</heading>
+                    </fragment>
+                </section>
+                <section id="details">
+                    <properties-fragment xmlns:psof="http://www.pageseeder.org/function" id="2">
+                        <!-- <property name="admin-name" title="Admin contact name" value="{self.CONTACT_NAME}"/>
+                        <property name="admin-email" title="Admin contact email" value="{self.CONTACT_EMAIL}"/>
+                        <property name="admin-phone" title="Admin contact phone" value="{self.CONTACT_PHONE}"/> -->
                     </properties-fragment>
                 </section>
             </document>''')
