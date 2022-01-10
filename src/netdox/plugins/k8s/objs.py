27e1f6a3f6d62f87b2d8fb00538fe2fbe54d673a
Linus Kirkwood <lkirkwood@allette.com.au>
2022-01-10 18:20:15 +1100

    added some dataclasses

--- src/netdox/plugins/k8s/objs.py
+++ src/netdox/plugins/k8s/objs.py
@@ -8,7 +8,38 @@ from netdox.nodes import ProxiedNode
 from dataclasses import dataclass
 
 @dataclass
+class Deployment:
+    """K8s deployment."""
+    name: str
+    """Name of this deployment."""
+    labels: dict[str, str]
+    """Some labels applied to this deployment."""
+    containers: list[Container]
+    """The containers specified by this deployment."""
+
+@dataclass
+class Container:
+    """Docker container."""
+    name: str
+    """Name of this container."""
+    image: str
+    """URI of the image this container runs."""
+    volumes: list[MountedVolume]
+   
+@dataclass(frozen = True)
+class MountedVolume:
+    """Volume from a persistent volume claim mounted in a Container."""
+    pvc: str
+    """Name of the persistent volume claim this volume is from."""
+    sub_path: str
+    """Path to the mount point in PVC."""
+    mount_path: str
+    """Path to the mount point in container."""
+
+
+@dataclass
 class Pod:
+    """K8s pod."""
     name: str
     """Name of this pod."""
     workerName: str
@@ -39,7 +70,7 @@ class App(ProxiedNode):
     Only includes paths starting on domains that also resolve to a configured proxy IP."""
     pod_labels: dict
     """Labels applied to the pods"""
-    template: dict
+    template: list[Container]
     """Template pods are started from"""
     pods: list[Pod]
     """A list of pods running this app"""
@@ -53,16 +84,13 @@ class App(ProxiedNode):
             cluster: str, 
             paths: Iterable[str] = None,
             labels: dict = None, 
-            pods: list[Pod] = None, 
-            template: dict = None
+            pods: Iterable[Pod] = None, 
+            template: Iterable[Container] = None
         ) -> None:
-
+        proxies = [proxy for proxy in utils.config('k8s')[cluster]['proxies']]
         domains = {p.split('/')[0] for p in sorted(paths if paths else [], key = len)}
         for domain in list(domains):
-            if not any([
-                network.resolvesTo(domain, proxy) for proxy 
-                in utils.config('k8s')[cluster]['proxies']
-            ]):
+            if not any([network.resolvesTo(domain, proxy) for proxy in proxies]):
                 domains.remove(domain)
 
         super().__init__(
@@ -76,8 +104,8 @@ class App(ProxiedNode):
         self.paths = set(paths) if paths else set()
         self.cluster = cluster
         self.pod_labels = labels or {}
-        self.pods = pods or []
-        self.template = template or {}
+        self.pods = list(pods) if pods else []
+        self.template = list(template) if template else []
 
     ## abstract properties
     
@@ -91,26 +119,26 @@ class App(ProxiedNode):
     def psmlPodTemplate(self) -> Tag:
         section = Tag(is_xml=True, name='section', attrs={'id':'template', 'title':'Pod Template'})
         count = 0
-        for container, template in self.template.items():
+        for container in self.template:
             frag = psml.PropertiesFragment(id = 'container_' + str(count), properties = [
-                    psml.Property(name = 'container', title = 'Container Name', value = container),
-                    psml.Property(name = 'image', title = 'Image ID', value = template['image'])
+                psml.Property(name = 'container', title = 'Container Name', value = container.name),
+                psml.Property(name = 'image', title = 'Image ID', value = container.image)
             ])
-            for volume, paths in self.template[container]['volumes'].items():
+            for volume in container.volumes:
                 frag.append(psml.Property(
                     name = 'pvc', 
                     title = 'Persistent Volume Claim', 
-                    value = volume
+                    value = volume.pvc
                 ))
                 frag.append(psml.Property(
                     name = 'mount_path', 
                     title = 'Path in Container', 
-                    value = paths['mount_path']
+                    value = volume.mount_path
                 ))
                 frag.append(psml.Property(
                     name = 'sub_path', 
                     title = 'Path in PVC', 
-                    value = paths['sub_path']
+                    value = volume.sub_path
                 ))
 
             section.append(frag)
