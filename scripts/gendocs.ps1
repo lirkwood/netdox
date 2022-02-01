9217be6e425290661db0bd2bdb70c9e6369e3707
Linus Kirkwood <lkirkwood@allette.com.au>
2022-02-01 17:18:32 +1100

    updated gendocs script

--- scripts/gendocs.ps1
+++ scripts/gendocs.ps1
@@ -1,7 +1,7 @@
 Push-Location
 Set-Location "$PSScriptRoot\..\docs"
-Remove-Item .\_source\source\*
-Remove-Item -Recurse .\_build\apidoc\*
+if (Test-Path ".\_source\source\") { Remove-Item .\_source\source\* }
+if (Test-Path ".\_build\apidoc\*") { Remove-Item -Recurse .\_build\apidoc\* }
 sphinx-apidoc --implicit-namespaces -M -T -o .\_source\source\ ..\src\netdox\
 sphinx-build -Ea -b psml .\_source\ .\_build\apidoc
 Pop-Location
--- scripts/gendocs.sh
+++ scripts/gendocs.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-pushd
-cd dirname "$0/../docs"
-rm -f _source/source/*
-rm -rf _build/apidoc
+pushd .
+cd $(dirname "$0")"/../docs"
+[[ -d '_source/source' ]] && rm -f '_source/source/*'
+[[ -d '_build/apidoc' ]] && rm -rf '_build/apidoc'
 sphinx-apidoc --implicit-namespaces -M -T -o _source/source ../src/netdox
 sphinx-build -Ea -b psml _source _build/apidoc
 popd
