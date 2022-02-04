9352085158878d93271ff67cc5535b69f91af5ad
Linus Kirkwood <lkirkwood@allette.com.au>
2022-02-04 18:06:09 +1100

    updated version for release

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     long_description_content_type = 'text/markdown',
     author = 'Linus Kirkwood',
     author_email = 'linuskirkwood@gmail.com',
-    version = '1.3.1',
+    version = '1.3.2',
     url = 'https://netdox.allette.com.au/',
     download_url = 'https://gitlab.allette.com.au/allette/devops/network-documentation',
     packages = find_namespace_packages(where = 'src'),
