ee699c9617bba673c7046d68ef6d6108a3ba4846
Linus Kirkwood <linuskirkwood@gmail.com>
2022-05-13 17:57:33 +1000

    renamed NetworkManager to pluginmanager and added app/lifecycle

---
+++ src/netdox/app.py
@@ -0,0 +1,386 @@
+from __future__ import annotations
+
+import importlib
+import json
+import logging
+import os
+import pkgutil
+from enum import Enum
+import shutil
+from traceback import format_exc
+from types import ModuleType
+from typing import Callable, Iterator, Optional
+
+from netdox import config, containers, utils
+from netdox.helpers import LabelDict
+from netdox.nodes import Node
+from netdox import pageseeder
+
+logger = logging.getLogger(__name__)
+
+class LifecycleStage(Enum):
+    INIT = 0
+    DNS = 1
+    NAT = 2
+    NODES = 3
+    FOOTERS = 4
+    WRITE = 5
+    CLEANUP = 6
+
+class App:
+    
+    def init(self) -> None:
+        """
+        Removes old, populated output directories and recreates them.
+        """
+        pageseeder.clear_loading_zone()
+
+        if not os.path.exists(utils.APPDIR+ 'out'):
+            os.mkdir(utils.APPDIR+ 'out')
+        # remove old output files
+        for folder in os.scandir(utils.APPDIR+ 'out'):
+            if folder.is_dir():
+                shutil.rmtree(folder)
+            else:
+                os.remove(folder)
+        
+        for outfolder in utils.OUTDIRS:
+            os.mkdir(utils.APPDIR+ 'out'+ os.sep+ outfolder)
+
+    def _valid_config(self, plugin_attrs: set[str]) -> config.NetworkConfig:
+        """
+        Fetches the config from PageSeeder, but performs some validation
+        before returning it.
+
+        If the config has incorrect attributes specified for a label,
+        the template will be updated, and a valid config created.
+        This config will be serialised for the upload.
+
+        :param plugin_attrs: Set of attributes that should be configured for each label.
+        :type plugin_attrs: set[str]
+        :return: _description_
+        :rtype: config.NetworkConfig
+        """
+        cfg = config.NetworkConfig.from_pageseeder()
+        # TODO find solution for config vals being dropped when the plugin is disabled
+        if cfg.is_empty or (not cfg.normal_attrs) or (plugin_attrs - cfg.attrs):
+            logger.warning('Updating config template on PageSeeder.')
+            config.update_template(plugin_attrs)
+            cfg.update_attrs(plugin_attrs)
+            with open(utils.APPDIR+ 'out/config.psml', 'w') as stream:
+                stream.write(cfg.to_psml())
+        return cfg
+
+    def refresh(self, dry: bool = False) -> None:
+
+        if dry: logger.info('Refresh running as dry run: no documents will be uploaded.')
+
+        # Initialisation                                                    #
+
+        self.init()
+        plugin_mgr = PluginManager()
+        network = containers.Network(
+            config = self._valid_config(plugin_mgr.pluginAttrs), 
+            labels = LabelDict.from_pageseeder()
+        )
+
+        plugin_mgr.runStage(network, LifecycleStage.INIT)
+
+        #-------------------------------------------------------------------#
+        # Primary data-gathering stages                                     #
+        #-------------------------------------------------------------------#
+        
+        plugin_mgr.runStage(network, LifecycleStage.DNS)
+        plugin_mgr.runStage(network, LifecycleStage.NAT)
+        plugin_mgr.runStage(network, LifecycleStage.NODES)
+
+        #-------------------------------------------------------------------#
+        # Generate objects for unused private IPs in used subnets,          #
+        # run any pre-write plugins                                         #
+        #-------------------------------------------------------------------#
+
+        network.ips.fillSubnets()
+        plugin_mgr.runStage(network, LifecycleStage.FOOTERS)
+
+        #-------------------------------------------------------------------#
+        # Write domains, ips, and nodes to pickle and psml,                 #
+        # scan for stale files and generate report,                         #
+        # and run any post-write plugins                                    #
+        #-------------------------------------------------------------------#
+
+        network.dump()
+        network.writePSML()
+        plugin_mgr.runStage(network, LifecycleStage.WRITE)
+        
+        network.report.addSection(
+            utils.stale_report(pageseeder.findStale(utils.OUTDIRS)))
+        with open(utils.APPDIR + 'src/warnings.log', 'r') as stream:
+            network.report.logs = stream.read()
+        network.report.writeReport()
+
+        #-------------------------------------------------------------------#
+        # Zip, upload, and cleanup                                          #
+        #-------------------------------------------------------------------#
+
+        zip = shutil.make_archive(utils.APPDIR+ 'src/netdox-psml', 'zip', utils.APPDIR + 'out')
+        if not dry:
+            pageseeder.zip_upload(zip, 'website')
+        else:
+            logger.warning('Did not upload documents due to --dry-run flag.')
+
+        plugin_mgr.runStage(network, LifecycleStage.CLEANUP)
+
+        logger.info('Done.')
+
+
+class PluginManager:
+    """
+    Discovers plugins.
+    """
+    namespace: ModuleType
+    """The namespace package to load plugins from."""
+    DEFAULT_NAMESPACE = 'netdox.plugins'
+    """Name of the namespace package to load by default."""
+    plugins: set[Plugin]
+    """Set of loaded plugins."""
+    loaded: list[str]
+    """List of the names of loaded plugins."""
+    nodemap: dict[type[Node], Plugin]
+    """Maps the subclasses of Node that a plugin exports to the module object."""
+    enabled: PluginWhitelist
+    """List of plugins enabled by the user."""
+
+    def __init__(self, 
+            namespace: ModuleType = None, 
+            whitelist: list[str] = None
+        ) -> None:
+        """
+        Constructor.
+
+        :param namespace: Namespace to load plugins from, defaults to DEFAULT_NAMESPACE
+        :type namespace: ModuleType, optional
+        :param whitelist: List of plugin names to enable.
+        If equal to PluginWhitelist.WILDCARD (["*"]), all plugins are enabled.
+        If not set, defaults to the value in the config file. 
+        If the file is missing or empty, falls back to wildcard.
+        :type whitelist: list[str], optional
+        :param network: existing Network object to use, defaults to None.
+        :type network: Network, optional
+        """
+        # Initialisation
+        self.plugins = set()
+        self.loaded = []
+        self.nodemap = {}
+        self.enabled = PluginWhitelist(whitelist) if whitelist else self._load_whitelist()
+        self.namespace = namespace or importlib.import_module(self.DEFAULT_NAMESPACE)
+        self.loadPlugins()
+
+        # Reporting
+        if self.enabled.is_wildcard:
+            logger.warning('Plugin whitelist is wildcard. All plugins will be enabled.')
+        else:
+            for plugin in self.enabled:
+                if plugin not in [_plugin.name for _plugin in self.plugins]:
+                    logger.warning(f"Plugin '{plugin}' is enabled but was not found.")
+
+        if self.plugins:
+            logger.info(f"NetworkManager discovered the following plugins in '{self.namespace.__name__}': "
+                + json.dumps([plugin.name for plugin in self.plugins], indent = 2))
+        else:
+            logger.warning(f"Failed to discover any plugins in '{self.namespace.__name__}'")
+
+    ## Plugin methods
+
+    def add(self, plugin: Plugin) -> None:
+        """
+        Adds a plugin to the pluginmap
+
+        :param plugin: An instantiated subclass of Plugin
+        :type plugin: Plugin
+        """
+        self.plugins.add(plugin)
+
+        for node_type in plugin.node_types:
+            self.nodemap[node_type] = plugin
+
+    def loadPlugins(self) -> None:
+        """
+        Scans a namespace for valid python modules and imports them.
+
+        :param dir: The namespace to scan for plugins
+        :type dir: str
+        :raises ImportError: If an Exception is raised during the call to 
+        ``importlib.import_module``
+        """
+        modules = list(pkgutil.iter_modules(getattr(self.namespace, '__path__', ())))
+        for module in modules:
+            logger.debug(f'Discovered module {module.name}')
+            if (module.name in self.enabled and 
+                module.name not in self.loaded):
+                self._loadPlugin(module.name)
+
+    def _loadPlugin(self, name: str) -> bool:
+        """
+        Loads a plugin from it's name.
+
+        :param name: Name of the plugin to load.
+        :type name: str
+        :return: 
+        :rtype: bool
+        """
+        try:
+            plugin = Plugin(importlib.import_module(
+                self.namespace.__name__ +'.'+ name))
+
+            if not self.validate_deps(plugin):
+                raise ImportError(
+                    f'Failed to load dependecies for {plugin.name}')
+
+            self.add(plugin)
+            return True
+
+        except Exception:
+            logger.error(
+                f'Failed to import {name}:\n{format_exc()}')
+        return False
+
+    def validate_deps(self, plugin: Plugin) -> bool:
+        """
+        Returns True if all of the dependencies for *plugin* are present in *names*.
+        False otherwise.
+
+        :param plugin: The plugin to validate the dependencies of.
+        :type plugin: Plugin
+        :param names: A list of names of available plugins.
+        :return: True if plugin can run, else False.
+        :rtype: bool
+        """
+        failed = set()
+        for dep in plugin.dependencies:
+            if dep not in self.enabled and dep not in self.loaded:
+                if not self._loadPlugin(dep):
+                    failed.add(dep)
+        return not bool(failed)
+
+    def initPlugins(self) -> None:
+        """
+        Initialises all plugins in the pluginmap.
+        """
+        for plugin in self.plugins:
+            try:
+                plugin.init()
+            except Exception:
+                logger.error(
+                    f'{plugin.name} threw an exception during initialisation: \n{format_exc()}')
+
+    def runPlugin(self, 
+            network: containers.Network, 
+            plugin: Plugin, 
+            stage: LifecycleStage
+        ) -> None:
+        """
+        Runs the registered method of *plugin* for *stage*.
+
+        :param network: The Network object to populate.
+        :type network: containers.Network
+        :param plugin: The plugin module to run.
+        :type plugin: ModuleType
+        :param stage: The current stage.
+        :type stage: LifecycleStage, optional
+        """
+        try:
+            logger.debug(f'Running plugin {plugin.name} stage {stage}')
+            plugin.stages[stage](network)
+        except Exception:
+            logger.error(f'{plugin.name} threw an exception during stage {stage}: \n{format_exc()}')
+
+    def runStage(self, network: containers.Network, stage: LifecycleStage) -> None:
+        """
+        Runs all the plugins in a given stage
+
+        :param network: The Network object to populate.
+        :type network: containers.Network
+        :param stage: The stage to check for plugins
+        :type stage: LifecycleStage
+        """
+        logger.info(f'Starting stage: {stage}')
+        for plugin in self.plugins:
+            if stage in plugin.stages:
+                self.runPlugin(network, plugin, stage)
+
+    @property
+    def pluginAttrs(self) -> set[str]:
+        """
+        Returns a set of label-configurable attributes provided by plugins.
+
+        :return: A set of strings to be used as property names in the config.
+        :rtype: set[str]
+        """
+        return {
+            attr for plugin in {
+                plugin for plugin in self.plugins
+            } for attr in getattr(plugin, '__attrs__', ())
+        }
+
+    def _load_whitelist(self) -> PluginWhitelist:
+        """
+        Returns a PluginWhitelist from the config file or a wildcard.
+
+        :return: A PluginWhitelist instance.
+        :rtype: PluginWhitelist
+        """
+        try:
+            with open(utils.APPDIR+ 'cfg/plugins.json', 'r') as stream:
+                return PluginWhitelist(json.load(stream))
+        except FileNotFoundError:
+            logger.warning('Plugin configuration file is missing from: ' +
+                os.path.realpath(os.path.join(utils.APPDIR, 'cfg/plugins.json')))
+            return PluginWhitelist(PluginWhitelist.WILDCARD)
+        except Exception:
+            logger.warning('Unable to load plugin configuration file.')
+            return PluginWhitelist(PluginWhitelist.WILDCARD)
+
+class PluginWhitelist(list):
+    WILDCARD = ["*"]
+    """Constant that matches every plugin."""
+    @property
+    def is_wildcard(self) -> bool:
+        return self == self.WILDCARD
+
+    def __contains__(self, key) -> bool:
+        if self.is_wildcard:
+            return True
+        return super().__contains__(key)
+
+    def __iter__(self) -> Iterator[str]:
+        if self.is_wildcard:
+            return ().__iter__()
+        return super().__iter__()
+
+
+class Plugin:
+    module: ModuleType
+    """The imported plugin module object."""
+    name: str
+    """Name of this plugin."""
+    stages: dict[LifecycleStage, Callable[[containers.Network], None]]
+    """A dict mapping stages to a callable accepting a Network."""
+    config: Optional[dict]
+    """A dictionary of configuration values for this plugin.
+    Will be used in the config template."""
+    dependencies: set[str]
+    """A set of plugin names this plugin depends on."""
+    node_types: list[type]
+    """A list of the Node subclasses that this plugin exports."""
+
+    def __init__(self, module: ModuleType) -> None:
+        self.module = module
+        self.name = module.__name__.split('.')[-1]
+        self.stages = getattr(module, '__stages__')
+        self.config = getattr(module, '__config__', None)
+        self.dependencies = getattr(module, '__depends__', set())
+        self.node_types = getattr(module, '__nodes__', [])
+
+    def init(self) -> None:
+        """Performs any required initialisation for the plugin."""
+        getattr(self.module, 'init', lambda: None)()
