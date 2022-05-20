5f7259bdc7cc8365b11018adba931e50f6025ef0
Linus Kirkwood <lkirkwood@allette.com.au>
2022-05-20 13:57:50 +1000

    added test for generate_template

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,5 +1,16 @@
-from netdox import config
+from bs4 import BeautifulSoup
+from netdox import config, psml
 import pytest
+
+def test_generate_template():
+    attrs = {'attr1', 'attr2', 'attr3'}
+    template = BeautifulSoup(config.generate_template(attrs), 'xml')
+    frag_dict = psml.PropertiesFragment.from_tag(
+        template.find('t:fragment', type = 'label').find('properties-fragment')
+    ).to_dict()
+
+    assert {'label': ''} | dict.fromkeys(attrs, '') == frag_dict
+
 
 class TestNetworkConfig:
 
