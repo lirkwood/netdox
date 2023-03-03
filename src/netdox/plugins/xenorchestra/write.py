4a5ba3d6f5a2dc6ec05471fae91e739a25155675
Linus Kirkwood <lkirkwood@allette.com.au>
2023-03-03 14:33:20 +1100

    rewrote xo publication to use list of pools

--- src/netdox/plugins/xenorchestra/write.py
+++ src/netdox/plugins/xenorchestra/write.py
@@ -8,46 +8,38 @@ from bs4 import BeautifulSoup, Tag
 from lxml import etree
 from netdox import pageseeder
 from netdox import Network
-from netdox.plugins.xenorchestra.objs import VirtualMachine
+from netdox.plugins.xenorchestra.objs import VirtualMachine, Pool
 from netdox.utils import APPDIR
 
 logger = logging.getLogger(__name__)
 
 
-def genpub(network: Network, pubdict: dict[str, dict[str, list[str]]]) -> None:
+def genpub(network: Network, pools: list[Pool]) -> None:
     """
     Generates a publication linking pools to hosts to vms
 
     :param network: The network
     :type network: Network
-    :param pubdict: A dictionary of node docids
-    :type pubdict: dict[str, dict[str, list[Node]]]
-    """
+    :param pools: A list of pools.
+    :type pools: list[Pool]
+   """
     pub = BeautifulSoup(PUB, 'xml')
     section = pub.find('section', id = 'pools')
-    count = 0
-    for pool, hosts in pubdict.items():
+
+    for poolnum, pool in enumerate(pools):
         heading = pub.new_tag('heading', level = 2)
-        heading.string = pool
+        heading.string = pool.name
         section.append(heading)
-        heading.wrap(pub.new_tag('fragment', id = f'heading_{count}'))
+        heading.wrap(pub.new_tag('fragment', id = f'heading_{poolnum}'))
 
-        xfrag = pub.new_tag(name = 'xref-fragment', id = f'pool_{count}')
-        for hostip, uuids in hosts.items():
-            node = network.ips[hostip].node
-            if node:
-                xfrag.append(pub.new_tag('blockxref', frag = 'default', type = 'embed', docid = node.docid))
-            else:
-                placeholder = pub.new_tag('para')
-                placeholder.string = hostip
-                xfrag.append(placeholder)
+        xfrag = pub.new_tag(name = 'xref-fragment', id = f'pool_{poolnum}')
+        for host in pool.hosts.values():
+            xfrag.append(pub.new_tag('blockxref', frag = 'default', type = 'embed', docid = host.node.docid))
 
-            for uuid in uuids:
-                vm = network.nodes[uuid]
+            for vm in host.vms.values():
                 xfrag.append(pub.new_tag('blockxref', frag = 'default', type = 'embed', docid = vm.docid, level = 1))
                 
         section.append(xfrag)
-        count += 1
 
     with open(APPDIR+ 'plugins/xenorchestra/src/xopub.psml', 'w') as stream:
         stream.write(pub.prettify())
