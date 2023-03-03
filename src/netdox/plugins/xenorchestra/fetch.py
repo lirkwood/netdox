7c179c3f5baafb96ddf00b557c34b8e9a9f191d0
Linus Kirkwood <lkirkwood@allette.com.au>
2023-03-03 14:33:09 +1100

    rewrote fetch to use host and pool

--- src/netdox/plugins/xenorchestra/fetch.py
+++ src/netdox/plugins/xenorchestra/fetch.py
@@ -7,13 +7,12 @@ Provides a function which links records to their relevant XenOrchestra VMs and g
 This script is used during the refresh process to link DNS records to the VMs they resolve to, and to trigger the generation of a publication which describes all VMs, their hosts, and their host's pool.
 """
 import asyncio
-import json
 import logging
 
-from netdox import iptools, utils
-from netdox import IPv4Address, Network
-from netdox.nodes import DefaultNode, PlaceholderNode
-from netdox.plugins.xenorchestra.objs import XOServer, VirtualMachine
+from netdox import utils
+from netdox import Network
+from netdox.nodes import DefaultNode
+from netdox.plugins.xenorchestra.objs import XOServer, Pool, Host, VirtualMachine
 from datetime import datetime
 
 logger = logging.getLogger(__name__)
@@ -22,128 +21,70 @@ logger = logging.getLogger(__name__)
 # User functions #
 ##################
 
-def runner(network: Network) -> dict[str, dict[str, list[str]]]:
+def runner(network: Network) -> list[Pool]:
     """
     Generates VirtualMachine and Host instances and adds them to the network.
+    
+    :param network: The network
+    :type network: Network
+    :return: A list of Pool objects.
+    :rtype: list[Pool]
+    """
+    return asyncio.run(get_vms(network))
+    
+
+async def get_vms(network: Network) -> list[Pool]:
+    """
+    Gets VM info from XenOrchestra.
 
     :param network: The network.
     :type network: Network
+    :return: Dict mapping host machine IPs to their hosted VMs.
+    :rtype: dict[str, list[VirtualMachine]]
     """
-    # Generate XO Docs
-    vms, hostVMs, poolHosts = asyncio.run(makeNodes(network))
+    async with XOServer(**utils.config('xenorchestra')) as xo:
+        pool_data = xo.fetchObjs({'type': 'pool'})
+        host_data = xo.fetchObjs({'type': 'host'})
+        vm_data = xo.fetchObjs({'type': 'VM'})
+        snapshot_data = await xo.fetchObjs({'type': 'VM-snapshot'})
 
-    pubdict = {
-        pool: {
-            hostip: [
-                vmuuid for vmuuid in hostVMs[hostip]
-            ]
-            for hostip in hostlist 
-        } 
-        for pool, hostlist in poolHosts.items()
-    }
+        pools: dict[str, Pool] = {}
+        for uuid, data in (await pool_data).items():
+            pools[uuid] = Pool(uuid, data['name_label'], {})
+        
+        for uuid, data in (await host_data).items():
+            node = DefaultNode(network, name = data['name_label'], private_ip = data['address'])
+            pools[data['$pool']].hosts[uuid] = (Host(uuid, data['name_label'], node, {}))
+
+        # snapshot_data = (await snapshot_data)
+        for uuid, data in (await vm_data).items():
+            if data['power_state'] != 'Running':
+                continue
+
+            if 'mainIpAddress' not in data:
+                logger.warning(f'VM {data["name_label"]} has no IP address')
+                continue
+
+            snapshot_dts = []
+            if 'snapshots' in data:
+                for snapshot_id in data['snapshots']:
+                    snapshot_dts.append(datetime.fromtimestamp(snapshot_data[snapshot_id]['snapshot_time']))                    
+
+            pool = pools[data['$pool']]
+            host = pool.hosts[data['$container']]
+
+            host.vms[uuid] = VirtualMachine(
+                network = network,
+                name = data['name_label'],
+                desc = data['name_description'],
+                uuid = uuid,
+                template = data['other']['base_template_name'] if 'base_template_name' in data['other'] else '—',
+                os = data['os_version'],
+                host = host.node.private_ip,
+                pool = pool.name,
+                snapshots = snapshot_dts,
+                private_ip = data['mainIpAddress'],
+                tags = data['tags']
+            )
     
-    return pubdict
-
-async def makeNodes(network: Network) -> tuple[dict, dict[str, list[str]], dict[str, list[str]]]:
-    """
-    Fetches info about pools, hosts, and VMs
-
-    :param network: The network
-    :type network: Network
-    """
-    # TODO rework this whole module to take advantage of XOServer
-    async with XOServer(**utils.config('xenorchestra')) as xo:
-        pools = await xo.fetchObjs({'type': 'pool'})
-        hosts = await xo.fetchObjs({'type': 'host'})
-        vms = await xo.fetchObjs({'type': 'VM'})
-        snapshots = await xo.fetchObjs({'type': 'VM-snapshot'})
-        
-    # Pools
-    poolNames: dict[str, str] = {}
-    poolHosts: dict[str, list[str]] = {}
-    for uuid, pool in pools.items():
-        poolNames[uuid] = pool['name_label']
-        poolHosts[pool['name_label']] = []
-
-
-    # Hosts
-    hostVMs: dict[str, list[str]] = {}
-    for host in hosts.values():
-        hostVMs[host['uuid']] = []
-        poolHosts[poolNames[host['$pool']]].append(host['address'])
-        DefaultNode(network, name = host['name_label'], private_ip = host['address'])
-
-
-    # VMs
-    for uuid, vm in vms.items():
-        if vm['power_state'] == 'Running':
-            if 'mainIpAddress' in vm:
-                if iptools.valid_ip(vm['mainIpAddress']):
-
-                    if vm['mainIpAddress'] not in network.ips:
-                        IPv4Address(network, vm['mainIpAddress'])
-
-                    hostVMs[vm['$container']].append(vm['uuid'])
-
-                    snapshot_dts = []
-                    if 'snapshots' in vm:
-                        for snapshot_id in vm['snapshots']:
-                            snapshot_dts.append(datetime.fromtimestamp(snapshots[snapshot_id]['snapshot_time']))
-
-                    VirtualMachine(
-                        network = network,
-                        name = vm['name_label'],
-                        desc = vm['name_description'],
-                        uuid = uuid,
-                        template = vm['other']['base_template_name'] if 'base_template_name' in vm['other'] else '—',
-                        os = vm['os_version'],
-                        host = hosts[vm['$container']]['address'],
-                        pool = poolNames[vm['$pool']],
-                        snapshots = snapshot_dts,
-                        private_ip = vm['mainIpAddress'],
-                        tags = vm['tags']
-                    )
-
-                else:
-                    logger.warning(f'VM {vm["name_label"]} has invalid IPv4 address {vm["mainIpAddress"]}')
-            else:
-                logger.warning(f'VM {vm["name_label"]} has no IP address')
-
-    return vms, \
-        {hosts[hostid]['address']: vmlist for hostid, vmlist in hostVMs.items()}, \
-        poolHosts
-
-
-@utils.handle
-async def template_map(vms: dict):
-    """
-    Generates a PSML file of all objects that can be used to create a VM with ``createVM``.
-    This was for the deprecated webhook functionality.
-
-    :param vms: A dictionary of all the VMs, as returned by fetchType
-    :type vms: dict
-    """
-    vmSource: dict[str, dict[str, str]] = {
-        'vms': {},
-        'snapshots': {},
-        'templates': {}
-    }
-    async with XOServer(**utils.config('xenorchestra')) as xo:
-        templates = await xo.fetchObjs({'type': 'VM-template'})
-        snapshots = await xo.fetchObjs({'type': 'VM-snapshot'})
-
-    for vm in vms:
-        if vms[vm]['power_state'] == 'Running':
-            name = vms[vm]['name_label']
-            vmSource['vms'][name] = vm
-
-    for snapshot in snapshots:
-        name = snapshots[snapshot]['name_label']
-        vmSource['snapshots'][name] = snapshot
-        
-    for template in templates:
-        name = templates[template]['name_label']
-        vmSource['templates'][name] = template
-
-    with open(utils.APPDIR+ 'plugins/xenorchestra/src/templates.json', 'w', encoding='utf-8') as stream:
-        stream.write(json.dumps(vmSource, indent=2, ensure_ascii=False))
+    return list(pools.values())
