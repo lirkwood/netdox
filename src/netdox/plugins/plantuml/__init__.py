d12d9f5cd8e6002c580024b87cfe096354d06e76
Linus Kirkwood <linuskirkwood@gmail.com>
2023-08-16 12:42:06 +1000

    Updated logging behaviour

--- src/netdox/plugins/plantuml/__init__.py
+++ src/netdox/plugins/plantuml/__init__.py
@@ -5,9 +5,11 @@ from netdox.psml import image_fragment
 from netdox.utils import config
 from .diagram import OUTDIR
 
-from bs4 import BeautifulSoup
 from shutil import rmtree
 import os
+import logging
+
+logger = logging.getLogger(__name__)
 
 EXTRA_ATTRS = {
     Node: 'type',
@@ -25,8 +27,10 @@ def runner(network: Network) -> None:
     diagram_dir = f'/ps/{config()["pageseeder"]["group"].replace("-","/")}/website/diagrams'
     factory = NodeDiagramFactory(**config('plantuml'))
     for node in network.nodes:
+        logger.debug(f'Drawing node diagram for: {node.name}')
         factory.draw(node)
         node.psmlFooter.insert(image_fragment('diagram', f'{diagram_dir}/{node.docid}.svg'))
+    logger.debug('Finished drawing diagrams.')
 
 __stages__ = {
     LifecycleStage.INIT: init,
